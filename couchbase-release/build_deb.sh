ca97d5f8e41c348d7eec6c0205c06c5340617d50
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-01-18 12:43:18 -0800

    CBD-2280: New couchbase-release (based off build version) to support staging
    Change-Id: I5418df6e7c96e161229f1324c530adb3b65f340e
    Reviewed-on: http://review.couchbase.org/88016
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

---
+++ couchbase-release/build_deb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+this_dir=$(dirname $0)
+pushd ${this_dir}
+
+STAGING=$1
+
+if [[ "${STAGING}" == "yes" ]]; then
+    STAGE_EXT="-staging"
+else
+    STAGE_EXT=""
+fi
+
+VERSION=1.0
+RELEASE=4
+REL_NAME="couchbase-release${STAGE_EXT}-${VERSION}-${RELEASE}"
+
+rm -rf deb/${REL_NAME}
+
+sed -e "s/%STAGING%/${STAGE_EXT}/g" \
+    -e "s/%VERSION%/${VERSION}/g" \
+    -e "s/%RELEASE%/${RELEASE}/g" \
+    deb/tmpl/control.in > deb/debian_control_files/DEBIAN/control
+
+mkdir -p deb/debian_control_files/etc/apt/sources.list.d
+sed -e "s/%STAGING%/${STAGE_EXT}/g" deb/tmpl/couchbase.list.in \
+    > deb/debian_control_files/etc/apt/sources.list.d/couchbase.list
+
+cp -pr deb/debian_control_files deb/${REL_NAME}
+mkdir -p deb/${REL_NAME}/etc/apt/trusted.gpg.d
+cp -p GPG-KEY-COUCHBASE-1.0 deb/${REL_NAME}/etc/apt/trusted.gpg.d
+sudo chown -R root:root deb/${REL_NAME}
+dpkg-deb --build deb/${REL_NAME}
+sudo chown -R ${USER}:${USER} deb/${REL_NAME}
+
+popd
+
+cp ${this_dir}/deb/${REL_NAME}.deb ${REL_NAME}-amd64.deb
---
+++ couchbase-release/build_rpm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+this_dir=$(dirname $0)
+
+STAGING=$1
+
+if [[ "${STAGING}" == "yes" ]]; then
+    STAGE_EXT="-staging"
+else
+    STAGE_EXT=""
+fi
+
+VERSION=1.0
+RELEASE=5
+REL_NAME="couchbase-release${STAGE_EXT}-${VERSION}-${RELEASE}"
+
+rm -rf ~/rpmbuild
+mkdir ~/rpmbuild
+for dir in BUILD BUILDROOT RPMS SOURCES SPECS SRPMS; do
+    mkdir ~/rpmbuild/${dir}
+done
+
+pushd ${this_dir}/rpm
+
+sed -e "s/%STAGING%/${STAGE_EXT}/g" tmpl/couchbase-Base.repo.in \
+    > couchbase-Base.repo
+sed -e "s/%STAGING%/${STAGE_EXT}/g" \
+    -e "s/%VERSION%/${VERSION}/g"\
+    -e "s/%RELEASE%/${RELEASE}/g" \
+    tmpl/couchbase-release.spec.in > couchbase-release.spec
+
+cp -p ../GPG-KEY-COUCHBASE-1.0 ~/rpmbuild/SOURCES
+cp -p *.repo ~/rpmbuild/SOURCES
+rpmbuild -bb couchbase-release.spec
+
+popd
+
+cp ~/rpmbuild/RPMS/x86_64/${REL_NAME}.x86_64.rpm ${REL_NAME}-x86_64.rpm
