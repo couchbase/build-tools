40da10f19ba3c13576428d7a6a19a0a9e9611b96
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-03-06 15:49:31 -0800

    Testing
    Change-Id: Id12c97555159d077cb13ffb54c15d0a8e2e26f02
    Reviewed-on: http://review.couchbase.org/90563
    Reviewed-by: Kenneth Lareau <kenneth.lareau@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- sdk/nodejs/build.bat
+++ sdk/nodejs/build.bat
@@ -13,12 +13,9 @@ set "PATH=C:\node-v%NODE_VER%;%PATH%"
 
 cd couchnode
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" %MSVSARCH%
+rem call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" %MSVSARCH%
 
-call npm config set python "C:\Program Files\Python27\python.exe" || goto error
-call npm config set msvs_version 2017 || goto error
 call npm update || goto error
-call npm install --global --production windows-build-tools || goto error
 call npm install --ignore-scripts --unsafe-perm || goto error
 set npm_config_loglevel=silly
 node .\node_modules\prebuild\bin.js -b %NODE_VER% --verbose --force || goto error
