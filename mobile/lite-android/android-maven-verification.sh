0bad14bd7adfd88ea716ca593e0b9fcae6e99f94
Kim Trang <kim.trang@couchbase.com>
2018-07-09 17:52:44 -0700

    CBD-2576 Integrate gradle tasks as part of Jenkins's plugin
    Change-Id: I67d52d7066c5ca14b2617137cd486ee562a7832c
    Reviewed-on: http://review.couchbase.org/96712
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- mobile/lite-android/android-maven-verification.sh
+++ mobile/lite-android/android-maven-verification.sh
@@ -4,13 +4,9 @@ PRODUCT=$1
 RELEASE_VERSION=$2
 PUBLISH_URL=$3
 
-cd ${WORKSPACE}/build-tools/mobile/lite-android/maven-publish-verification
-gradle wrapper --gradle-version 4.8.1
-./gradlew --gradle-user-home ${WORKSPACE}/gradle_home build
-
 mkdir -p  ${WORKSPACE}/tmp
 pushd ${WORKSPACE}/tmp
-find ${WORKSPACE}/gradle_home/ -type f -name "couchbase*.aar"  |  xargs jar xvf
+find ${WORKSPACE}/caches -type f -name "couchbase*.aar"  |  xargs jar xvf
 
 # Error if grep returns 0 for CE packages
 for i in DatabaseEndpoint.class EncryptionKey.class; do
