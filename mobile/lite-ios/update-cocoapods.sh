dd594092ba35a9344a284308eccb483f362c203e
Kim Trang <kim.trang@couchbase.com>
2018-07-20 13:06:16 -0700

    CBD-2562 Add script to automate cocoapod provision files
    Change-Id: I09f0dcef1a33ca62fdfdc9b79d9305afd832682b
    Reviewed-on: http://review.couchbase.org/97209
    Reviewed-by: Kenneth Lareau <kenneth.lareau@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

---
+++ mobile/lite-ios/update-cocoapods.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# virtualenv stuff
+export LC_ALL=C
+export LANG=en_US.UTF-8
+pip3 install --user --upgrade virtualenv
+
+# start virtualenv
+/home/couchbase/.local/bin/virtualenv COCOAPODS || exit 1
+source ./COCOAPODS/bin/activate  || exit 1
+
+# Install required cocoapods
+gem install cocoapods --user-install
+gem which cocoapods
+
+# Checkout required repo
+cd ${WORKSPACE} && git clone git@github.com:couchbaselabs/couchbase-lite-ios-ee.git couchbase-lite-ios-ee
+
+cd ${WORKSPACE}/couchbase-lite-ios-ee/Podspecs
+declare -a JSON_FILES=( $(ls) )
+for fl in "${JSON_FILES[@]}"; do
+	python3 ${WORKSPACE}/build-tools/mobile/lite-ios/cocoapods-publish.py --version ${VERSION} --file $fl || exit 1
+done
+
+# deactivate virtualenv
+echo "Deactivating virtualenv ..."
+deactivate
+echo
