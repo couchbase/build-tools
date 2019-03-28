89f5c9d6e599022801f1e7d5f38cc53a2beec217
Kenneth Lareau <kenneth.lareau@couchbase.com>
2019-03-28 14:48:23 -0700

    Fix Unix build script for cbdep protoc-gen-go
    Rework to ensure even if the cbdep program is cached, it's copied to
    /tmp and given a 'chmod +x' to ensure it's runnable.
    
    Change-Id: Id6a6bbb7f2301c036d0a57113dfc434c23cd579f
    Reviewed-on: http://review.couchbase.org/106963
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- cbdeps/protoc-gen-go/protoc-gen-go_unix.sh
+++ cbdeps/protoc-gen-go/protoc-gen-go_unix.sh
@@ -6,19 +6,21 @@ DEPS=${WORKSPACE}/deps
 GO_VERS=1.11.5
 CBDEP_TOOL_VERS=0.9.3
 
-CBDEP_BIN_PATH=/home/couchbase/.cbdepscache/cbdep/${CBDEP_TOOL_VERS}/cbdep-${CBDEP_TOOL_VERS}-linux
-if [[ ! -f ${CBDEP_BIN_PATH} ]]; then
+CBDEP_BIN_CACHE=/home/couchbase/.cbdepscache/cbdep/${CBDEP_TOOL_VERS}/cbdep-${CBDEP_TOOL_VERS}-linux
+
+if [[ ! -f ${CBDEP_BIN_CACHE} ]]; then
     if [ $(uname -s) = "Darwin" ]; then
         CBDEP_URL=https://packages.couchbase.com/cbdep/${CBDEP_TOOL_VERS}/cbdep-${CBDEP_TOOL_VERS}-darwin
     else
         CBDEP_URL=https://packages.couchbase.com/cbdep/${CBDEP_TOOL_VERS}/cbdep-${CBDEP_TOOL_VERS}-linux
     fi
     curl -o /tmp/cbdep ${CBDEP_URL}
-    chmod +x /tmp/cbdep
-    /tmp/cbdep install -d "${DEPS}" golang ${GO_VERS}
 else
-   ${CBDEP_BIN_PATH} install -d "${DEPS}" golang ${GO_VERS}
+   cp ${CBDEP_BIN_CACHE} /tmp/cbdep
 fi
+
+chmod +x /tmp/cbdep
+/tmp/cbdep install -d "${DEPS}" golang ${GO_VERS}
 
 GOPATH=${WORKSPACE}
 PATH=${DEPS}/go1.11.5/bin:$PATH
