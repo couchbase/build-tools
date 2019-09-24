c8b762c9c160de00020c7eb0657ebd81062bb119
G. Blake Meike <blake.meike@couchbase.com>
2019-09-24 16:54:03 -0700

    Push mobile debug versions to S3 Change-Id: I254f000611099ecb28bd8185c23d188393577ee5 Reviewed-on: http://review.couchbase.org/115306 Reviewed-by: Chris Hillery <ceej@couchbase.com> Tested-by: G. Blake Meike <blake.meike@couchbase.com>

--- mobile/mobile-release-binaries-to-s3.sh
+++ mobile/mobile-release-binaries-to-s3.sh
@@ -180,7 +180,7 @@ get_s3_upload_link()
 }
 
 cd ${SRC_DIR}
-FILES=$(ls * | egrep -v 'source|\.xml|\.json|\.properties|\.md5*|\.sha*|test_coverage*|CHANGELOG|changes\.log|unsigned|debug|logtest|litetest')
+FILES=$(ls * | egrep -v 'source|\.xml|\.json|\.properties|\.md5*|\.sha*|test_coverage*|CHANGELOG|changes\.log|unsigned|logtest|litetest')
 TARGET_TMP_DIR=/tmp/${RELEASE}-${BLD_NUM}
 rm -rf ${TARGET_TMP_DIR} && mkdir -p ${TARGET_TMP_DIR}
 
