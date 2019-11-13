9e7000d667bda7fbd306a3de6b3a8e97f9fe7979
Chris Hillery <ceej@couchbase.com>
2019-11-13 02:51:41 -0800

    Re-organize Black Duck Jenkins scripts; add script for check-licenses job
    Change-Id: Ib605dcee38afb498ac1bec3802ace373f3f8c3b9
    Reviewed-on: http://review.couchbase.org/117795
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ blackduck/jenkins/check-licences/blackduck-check-licenses.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -ex
+
+git clone ssh://git@github.com/couchbase/license-reviews
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+cd ${WORKSPACE}/build-tools/blackduck/jenkins/check-licences
+pip3 install -r requirements.txt
+
+export LANG=en_US.UTF-8
+
+set +e
+./check-component-lic.py \
+    ${PRODUCT} ${VERSION} \
+    -c ~/.ssh/blackduck-creds.json \
+    -d ${WORKSPACE}/license-reviews
+retval=$?
+set -e
+
+cd ${WORKSPACE}/license-reviews
+git add ${PRODUCT} license-data
+git config --global push.default simple
+git diff --quiet && git diff --staged --quiet || git commit \
+    -m "Update report for ${PRODUCT} ${VERSION}" \
+    --author='Couchbase Build Team <build-team@couchbase.com>'
+git push
+
+exit $retval
