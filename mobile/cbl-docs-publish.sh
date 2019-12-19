0c3cbe65fae7899269042dd1c6982306d479a4c3
Ming Ho <ming.ho@couchbase.com>
2019-12-19 11:09:38 -0800

    DOC-6070  fix path issue for android doc also correct variable name for swift
    -Ming Ho
    
    Change-Id: Ifbf18600bd9530a85f7cc24081d0b1ba740da561
    Reviewed-on: http://review.couchbase.org/119603
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- mobile/cbl-docs-publish.sh
+++ mobile/cbl-docs-publish.sh
@@ -15,17 +15,18 @@ S3_URL="http://packages.couchbase.com/releases/${PRODUCT}/${VERSION}"
 if [[ ${PRODUCT} == 'couchbase-lite-ios' ]]; then
     PACKAGE_NAME_ARRAY=("couchbase-lite-objc-documentation_enterprise_${VERSION}.zip" "couchbase-lite-swift-documentation_enterprise_${VERSION}.zip")
 elif [[ ${PRODUCT} == 'couchbase-lite-android-ee' ]]; then
-    PACKAGE_NAME_ARRAY=("CouchbaseLite-${VERSION}-javadoc.jar")
+    PACKAGE_NAME_ARRAY=("${PRODUCT}-${VERSION}-javadoc.jar")
 fi
 
 echo "Publish to S3 ..."
 # Upload to S3 docs.couchbase.com
 for PKG in ${PACKAGE_NAME_ARRAY[@]}; do
+    echo "Publish ${PKG} to S3 ..."
     PKG_NAME=${PKG%.*} # remove file extentions as directory name
     S3_URL_couchbase_lite_java="s3://docs.couchbase.com/mobile/${VERSION}/couchbase-lite-java"
     if [[ ${PRODUCT} == 'couchbase-lite-android-ee' ]]; then
         curl ${S3_URL}/${PKG} -o CouchbaseLite-${VERSION}-javadoc.zip || exit 1
-        unzip CouchbaseLite-${VERSION}-javadoc.zip -d ${PKG_NAME} || exit 1
+        unzip CouchbaseLite-${VERSION}-javadoc.zip -d CouchbaseLite-${VERSION}-javadoc || exit 1
         #Android:
         s3cmd -c $S3CONFIG --acl-public -r put CouchbaseLite-${VERSION}-javadoc/ ${S3_URL_couchbase_lite_java}/ || exit 1
         s3cmd -c $S3CONFIG ls ${S3_URL_couchbase_lite_java}/
@@ -51,17 +52,17 @@ for PKG in ${PACKAGE_NAME_ARRAY[@]}; do
         done
         popd
     elif [[ ${PKG} == "couchbase-lite-swift-documentation_enterprise_${VERSION}.zip" ]]; then
-        S3_URL_couchbase_lite_objc="s3://docs.couchbase.com/mobile/${VERSION}/couchbase-lite-swift"
+        S3_URL_couchbase_lite_swift="s3://docs.couchbase.com/mobile/${VERSION}/couchbase-lite-swift"
         curl -LO ${S3_URL}/${PKG} || exit 1
         unzip ${PKG} -d ${PKG_NAME} || exit 1
         #Swift:
-        s3cmd -c $S3CONFIG --acl-public -r put couchbase-lite-swift-documentation_enterprise_${VERSION}/ ${S3_URL_couchbase_lite_objc}/  || exit 1
-        s3cmd -c $S3CONFIG ls ${S3_URL_couchbase_lite_objc}/
+        s3cmd -c $S3CONFIG --acl-public -r put couchbase-lite-swift-documentation_enterprise_${VERSION}/ ${S3_URL_couchbase_lite_swift}/  || exit 1
+        s3cmd -c $S3CONFIG ls ${S3_URL_couchbase_lite_swift}/
         echo "Re-uploading css file ..."
         pushd couchbase-lite-swift-documentation_enterprise_${VERSION}/
         CSS_FILES=$(find . -type f -name *.css |sed 's|^./||')
             for fl in ${CSS_FILES}; do
-                s3cmd -c $S3CONFIG --acl-public put -m "text/css" $fl ${S3_URL_couchbase_lite_objc}/$fl
+                s3cmd -c $S3CONFIG --acl-public put -m "text/css" $fl ${S3_URL_couchbase_lite_swift}/$fl
             done
         popd
     fi
