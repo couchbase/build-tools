7126299406e0574dd24c695e08d3e009f3d994fc
Ming Ho <ming.ho@couchbase.com>
2020-01-27 16:09:03 -0800

    CBD-2758, remove unecessary deps for sync_gateway -Ming Ho
    Change-Id: Ife96b0a543e894005985d4f0aa803bc1d14e5a20
    Reviewed-on: http://review.couchbase.org/121229
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- loc/sync_gateway/run_cloc.sh
+++ loc/sync_gateway/run_cloc.sh
@@ -5,7 +5,8 @@ RELEASE=$1
 shopt -s extglob
 
 # Third-party go code
-rm -rf godeps/src/github.com/!(couchbase*)
+rm -rf godeps/src/*/!(couchbase*)
+rm -rf godeps/src/github.com/couchbase/ns_server
 
 echo @@@@@@@@@@@@@@@@@@@@@@@@@
 echo "sync_gateway ${RELEASE}"
