fb520537cca8458c71945fe4b6514e8aa8031ddb
Chris Hillery <ceej@couchbase.com>
2020-03-03 00:49:38 -0800

    CBD-2985: Restore mbedtls for CBL ios/net Black Duck scans
    Change-Id: I2682366dd49f2b4facd8d67e1b81ee36080cccd4
    Reviewed-on: http://review.couchbase.org/123129
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/couchbase-lite-ios/prune_source.sh
+++ blackduck/couchbase-lite-ios/prune_source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
 #clean up unnecessary source directories
-for i in fleece gradle Docs doc test tests docs googletest third_party _obsolete mbedtls VisualStudio dotzlib MYUtilities; do
+for i in fleece gradle Docs doc test tests docs googletest third_party _obsolete VisualStudio dotzlib MYUtilities; do
    find . -type d -name $i | xargs rm -rf
 done
