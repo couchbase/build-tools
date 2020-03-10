97f72d2f5d5c307c8a8ce18848445700fa3a9803
Chris Hillery <ceej@couchbase.com>
2020-03-10 01:00:33 -0700

    CBD-3285: Release build-specific analytics jars
    Change-Id: Ic2f1ae31370d4fa36d370ee51befcd7c3ea347fc
    Reviewed-on: http://review.couchbase.org/123487
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ release/analytics-jars/release-analytics-jars.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+RELEASE=$1
+VERSION=$2
+BLD_NUM=$3
+
+# Download named build (use Ubuntu 18 - will need to update in far future
+# when we no longer support Ubuntu 18)
+curl -L http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-server/${RELEASE}/${BLD_NUM}/couchbase-server-enterprise_${VERSION}-${BLD_NUM}-ubuntu18.04_amd64.deb -o couchbase-server.deb
+
+# Extract jar contents
+ar x couchbase-server.deb
+tar xf data.tar.xz --wildcards --no-wildcards-match-slash --strip-components 5 './opt/couchbase/lib/cbas/repo/*.jar'
+pushd repo
+tar cvzf ../analytics-jars-${VERSION}-${BLD_NUM}.tar.gz *.jar
+popd
+
+# Publish to S3
+s3cmd -c ~/.ssh/live.s3cfg \
+  put -P analytics-jars-${VERSION}-${BLD_NUM}.tar.gz \
+  s3://packages.couchbase.com/releases/${VERSION}/
