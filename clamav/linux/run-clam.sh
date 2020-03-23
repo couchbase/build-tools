eadc6a6553ef9f4d1e70113679d65cad73c0c143
Chris Hillery <ceej@couchbase.com>
2020-03-23 01:58:33 -0700

    CBD-3228: Scripts for running ClamAV on Windows and Linux
    Change-Id: Ie41c5b85dc1778db87e96b1bfb76f13bf4bc94b8
    Reviewed-on: http://review.couchbase.org/122241
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ clamav/linux/run-clam.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ex
+
+# Update clamav database
+sudo freshclam
+
+# Download build
+echo "Downloading ${VERSION}-${BLD_NUM} ${PLATFORM} binary ..."
+PKG_NAME=${PRODUCT}-${EDITION}-${VERSION}-${BLD_NUM}-${PLATFORM}.x86_64.rpm
+LATESTBUILDS=http://latestbuilds.service.couchbase.com/builds/latestbuilds/${PRODUCT}/${RELEASE}/${BLD_NUM}/${PKG_NAME}
+curl --fail ${LATESTBUILDS} -o ${WORKSPACE}/${PKG_NAME} || exit 1
+
+echo "Extract ${VERSION}-${BLD_NUM} ${PLATFORM} binary ..."
+mkdir -p ${WORKSPACE}/scansrc
+pushd ${WORKSPACE}/scansrc
+/usr/bin/rpm2cpio ${WORKSPACE}/${PKG_NAME}  | /bin/cpio -idm || exit 1
+popd
+
+echo .................................
+echo ClamAV Version
+/usr/bin/clamscan -V
+echo .................................
+/usr/bin/clamscan  --follow-dir-symlinks=2 --recursive=yes --suppress-ok-results -l ${WORKSPACE}/scan.log  ${WORKSPACE}/scansrc  || exit 1
+
+grep 'Infected files: 0' ${WORKSPACE}/scan.log
+if [[ $? != 0 ]]; then
+    echo "ERROR!  Infected file(s) found!!"
+    exit 1
+fi
