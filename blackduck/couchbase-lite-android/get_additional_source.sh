81181c1efbd56e2dc8f00fd3c101a2d80ead72fc
Ming Ho <ming.ho@couchbase.com>
2020-03-30 15:37:38 -0700

    update ndk version to fix blackduck failure for CBL android.
    -Ming Ho
    
    Change-Id: I69a8e5929ad901393a141daf0d4bb1aa77b6d082
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/124544
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-android/get_additional_source.sh
+++ blackduck/couchbase-lite-android/get_additional_source.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 SDK_DIR=/home/couchbase/tools/android-sdk
-NDK_DIR=/home/couchbase/tools/android-ndk-r20
+NDK_DIR=/home/couchbase/tools/android-sdk/ndk/20.1.5948944
 if [ ! -f "local.properties" ]; then
     echo "ndk.dir=${NDK_DIR}" > local.properties
     echo "sdk.dir=${SDK_DIR}" >> local.properties
