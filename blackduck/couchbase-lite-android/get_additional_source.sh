fc6451d73ae9698b6f777cd9137adee90c4aa5ba
Ming Ho <ming.ho@couchbase.com>
2020-03-31 11:31:52 -0700

    CBD-3330: install ndk if it doesn't exist on BD jenkins slave.
    -Ming Ho
    
    Change-Id: Ia7a976b4c4c4243e6636c0038d9d3f3b738482f3
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/124630
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-android/get_additional_source.sh
+++ blackduck/couchbase-lite-android/get_additional_source.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -ex
-
+NDK_VERSION=$(cat couchbase-lite-android-ee/etc/jenkins/build.sh | grep ^NDK_VERSION |awk -F "\'|\"" '{print $2}')
 SDK_DIR=/home/couchbase/tools/android-sdk
-NDK_DIR=/home/couchbase/tools/android-sdk/ndk/20.1.5948944
+SDK_MGR=${SDK_DIR}/tools/bin/sdkmanager
+NDK_DIR=${SDK_DIR}/ndk/${NDK_VERSION}
+if [ ! -d ${NDK_DIR} ]; then
+  ${SDK_MGR} --install "ndk;${NDK_VERSION}"
+fi
 if [ ! -f "local.properties" ]; then
     echo "ndk.dir=${NDK_DIR}" > local.properties
     echo "sdk.dir=${SDK_DIR}" >> local.properties
