aee87cf0b680a4b12f49a83e6a6cab224b44ff29
Ming Ho <ming.ho@couchbase.com>
2020-04-17 10:30:52 -0700

    update BD script due to cbl android directory structure change.
    -Ming Ho
    
    Change-Id: I6533fd994ccd445b0105617127e32c85182699d6
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/126036
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-android/get_additional_source.sh
+++ blackduck/couchbase-lite-android/get_additional_source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-NDK_VERSION=$(cat couchbase-lite-android-ee/etc/jenkins/build.sh | grep ^NDK_VERSION |awk -F "\'|\"" '{print $2}')
+NDK_VERSION=$(cat cbl-java/etc/jenkins/check.sh | grep ^NDK_VERSION |awk -F "\'|\"" '{print $2}')
 SDK_DIR=/home/couchbase/tools/android-sdk
 SDK_MGR=${SDK_DIR}/tools/bin/sdkmanager
 NDK_DIR=${SDK_DIR}/ndk/${NDK_VERSION}
@@ -10,4 +10,4 @@ if [ ! -f "local.properties" ]; then
     echo "ndk.dir=${NDK_DIR}" > local.properties
     echo "sdk.dir=${SDK_DIR}" >> local.properties
 fi
-cp local.properties couchbase-lite-android-ee/local.properties
+cp local.properties cbl-java/local.properties
--- blackduck/couchbase-lite-android/prune_source.sh
+++ blackduck/couchbase-lite-android/prune_source.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 
 #clean up unnecessary source directories
-for i in fleece Docs doc test tests docs googletest third_party _obsolete; do
+for i in fleece Docs doc test tests docs googletest third_party _obsolete upgradetest cbbuild; do
      find . -type d -name $i | xargs rm -rf
 done
-rm -rf couchbase-lite-android/tools/upgradetest
