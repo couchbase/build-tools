bcb9a0506be482d81b7294d161b6c5ef6234d035
Ming Ho <ming.ho@couchbase.com>
2020-04-21 16:07:28 -0700

    PLEASE EDIT to provide a meaningful commit message!
    The following commits from your working branch will be included:
    
    commit 31ef6d92ea844d9747b49f9035ce808dd8143b0c
    Author: Ming Ho <ming.ho@couchbase.com>
    Date:   Tue Apr 21 15:34:47 2020 -0700
    
        fix BD scan for android.  need local.properties at CE level to scan
    
        -Ming Ho
    
    Change-Id: I61503b43cf9a970aa9796a9ae0a2848680f6f470
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/126355
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-android/get_additional_source.sh
+++ blackduck/couchbase-lite-android/get_additional_source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-NDK_VERSION=$(cat cbl-java/etc/jenkins/check.sh | grep ^NDK_VERSION |awk -F "\'|\"" '{print $2}')
+NDK_VERSION=$(cat cbl-java/ee/android/etc/jenkins/build.sh | grep ^NDK_VERSION |awk -F "\'|\"" '{print $2}')
 SDK_DIR=/home/couchbase/tools/android-sdk
 SDK_MGR=${SDK_DIR}/tools/bin/sdkmanager
 NDK_DIR=${SDK_DIR}/ndk/${NDK_VERSION}
@@ -11,3 +11,5 @@ if [ ! -f "local.properties" ]; then
     echo "sdk.dir=${SDK_DIR}" >> local.properties
 fi
 cp local.properties cbl-java/local.properties
+cp local.properties cbl-java/ce/android/local.properties
+cp local.properties cbl-java/ee/android/local.properties
--- blackduck/couchbase-lite-android/prune_source.sh
+++ blackduck/couchbase-lite-android/prune_source.sh
@@ -4,3 +4,7 @@
 for i in fleece Docs doc test tests docs googletest third_party _obsolete upgradetest cbbuild; do
      find . -type d -name $i | xargs rm -rf
 done
+
+rm -rf cbl-java/ee/java
+rm -rf cbl-java/ce/java
+rm -rf cbl-java/common/java
