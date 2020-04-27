2903478485023d81d70f6fb28181f83d0d469c12
Ming Ho <ming.ho@couchbase.com>
2020-04-27 14:23:59 -0700

    update BD config for CBL-java due to directory structure change.
    -Ming Ho
    
    Change-Id: I49679ba17a98d43a8fd11176a83360059ffbffb0
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/126745
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-java/prune_source.sh
+++ blackduck/couchbase-lite-java/prune_source.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -ex
 
+pwd
+
 #clean up unnecessary source directories
-for i in fleece Docs doc test tests docs googletest third_party; do
+for i in fleece Docs doc test tests docs googletest third_party cbbuild; do
      find . -type d -name $i | xargs rm -rf
 done
 
+###get rid of android codes which are not used by java
+rm -rf cbl-java/ee/android
+rm -rf cbl-java/ce/android
+rm -rf cbl-java/common/android
+
 #set local.properties for gradle
-echo "LINUX_LIBCXX_INCDIR=${LIBCXX_INCDIR}/c++/v1" > local.properties
-echo "LINUX_LIBCXX_LIBDIR=${LIBCXX_LIBDIR}" >> local.properties
+echo "LINUX_LIBCXX_INCDIR=${LIBCXX_INCDIR}/c++/v1" > cbl-java/ee/java/local.properties
+echo "LINUX_LIBCXX_LIBDIR=${LIBCXX_LIBDIR}" >> cbl-java/ee/java/local.properties
+
+echo "LINUX_LIBCXX_INCDIR=${LIBCXX_INCDIR}/c++/v1" > cbl-java/ce/java/local.properties
+echo "LINUX_LIBCXX_LIBDIR=${LIBCXX_LIBDIR}" >> cbl-java/ce/java/local.properties
