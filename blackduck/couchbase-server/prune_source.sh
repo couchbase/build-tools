fdd3dae29d1d5ef16e49595ccdeabaa578f4dcea
Chris Hillery <ceej@couchbase.com>
2020-05-12 18:23:08 -0700

    CBD-3365: More pruning and configuration for Black Duck Server scans
    Also tweaks so mad-hatter scanning will still work
    
    Change-Id: I687f58c0f8c8e9913672f76e4649dcc44ef6289a
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/127846
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/couchbase-server/prune_source.sh
+++ blackduck/couchbase-server/prune_source.sh
@@ -1,23 +1,46 @@
 #!/bin/bash -ex
 
-# Most of this stuff is npm-generated "compiled" code; we only want
-# to scan the npm package.json/package-lock.json files
-mv ns_server/priv/public/ui/package*.json ns_server/priv
-rm -rf ns_server/priv/public
+RELEASE=$1
 
-# cleanup unwanted stuff
+shopt -s extglob
+
+# NPM-related stuff - see CBD-3365
+
+if [ "$RELEASE" = "mad-hatter" ]; then
+    # Can't scan package.json files with NPM detector unless we also
+    # have package-lock.json, which we don't in mad-hatter, so...
+    rm ns_server/priv/public/ui/package.json
+    rm goproj/src/github.com/couchbase/cbgt/rest/static/lib/angular-bootstrap/package.json
+    rm goproj/src/github.com/couchbase/cbgt/rest/static/lib/angular-route/package.json
+    rm goproj/src/github.com/couchbase/cbgt/rest/static/lib/angular/package.json
+else
+    # Most of this stuff is npm-generated "compiled" code; we ONLY want
+    # to scan the npm package.json/package-lock.json files
+    mv ns_server/priv/public/ui/package*.json ns_server/priv
+    rm -rf ns_server/priv/public
+
+    for pkg in angular-bootstrap angular-route angular; do
+        rm -rf goproj/src/github.com/couchbase/cbgt/rest/static/lib/${pkg}/!(package*.json)
+    done
+fi
+
+# Server doesn't use asterix's dashboard, so prune that
+rm analytics/asterixdb/asterixdb/asterix-dashboard/src/node/package.json
+
+# Server doesn't use any of bleve-mapping-ui's NPM components, so eliminate them
+rm -rf godeps/src/github.com/blevesearch/bleve-mapping-ui/bower_components
+
+# END NPM-related stuff
+
+# cleanup unwanted stuff - test files, build scripts, etc.
 rm -rf testrunner
 rm -rf cbbuild
 rm -rf goproj/src/github.com/couchbase/query/data/sampledb
 rm -rf goproj/src/github.com/couchbase/docloader/examples
 rm -rf goproj/src/github.com/couchbase/indexing/secondary/docs
-
-# Ejecta
-rm -rf cbbuild/tools/iOS
-
-# rebar
 rm -f tlm/cmake/Modules/rebar
 
+# General-purpose removal of test data, examples, docs, etc.
 find . -name analytics -prune -o -type d -name test\* -print0 | xargs -0 rm -rf
 find . -name analytics -prune -o -type d -name testdata -print0 | xargs -0 rm -rf
 find . -name analytics -prune -o -type d -name gtest -print0 | xargs -0 rm -rf
@@ -30,26 +53,13 @@ find . -name analytics -prune -o -type d -name examples -print0 | xargs -0 rm -r
 find . -name analytics -prune -o -type d -name samples -print0 | xargs -0 rm -rf
 find . -name analytics -prune -o -type d -name benchmarks -print0 | xargs -0 rm -rf
 
-# QQQ See if asterixdb can update to include package-lock.json
-rm analytics/asterixdb/asterixdb/asterix-dashboard/src/node/package.json
-
-# QQQ See if cbgt can update to include package-lock.json
-rm goproj/src/github.com/couchbase/cbgt/rest/static/lib/angular-bootstrap/package.json
-rm goproj/src/github.com/couchbase/cbgt/rest/static/lib/angular-route/package.json
-rm goproj/src/github.com/couchbase/cbgt/rest/static/lib/angular/package.json
-
-# QQQ See if bleve can update to include package-lock.json
-rm godeps/src/github.com/blevesearch/bleve-mapping-ui/bower_components/angular/package.json
-rm godeps/src/github.com/blevesearch/bleve-mapping-ui/bower_components/angular-bootstrap/package.json
-rm godeps/src/github.com/blevesearch/bleve-mapping-ui/bower_components/bootstrap/package.json
-
 # godeps-specific pruning (hopefully eliminated after switching entirely to Go modules)
 rm -rf godeps/src/golang.org/x/tools/cmd/heapview/client
 
 # Remove all msvc, vcs* window projects
 WIN='example *msvc* *vcproj* *vcxproj* visual vstudio dot_net_example example csharp vc7ide'
 for windir in ${WIN}; do
-	find . -name analytics -prune -o -type d -name "$windir" -print0 | xargs -0 rm -rf
+    find . -name analytics -prune -o -type d -name "$windir" -print0 | xargs -0 rm -rf
 done
 
 # cbdeps-specific pruning
