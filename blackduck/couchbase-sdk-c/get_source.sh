36e665fc9c7e0657fd602374b1be131cee38bd9c
Chris Hillery <ceej@couchbase.com>
2020-05-26 19:12:42 -0700

    CBD-3321: Use Black Duck manifest for unshipped third-party components
    Change-Id: I4cc8dbdbda3fb13780d58e99dcbaeecf798b99f1
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/129028
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/couchbase-sdk-c/get_source.sh
+++ blackduck/couchbase-sdk-c/get_source.sh
@@ -15,8 +15,3 @@ else
     echo "No tag $VERSION, assuming master"
 fi
 popd
-# These are probably wrong per-version, but it's all we've got
-git clone --branch OpenSSL_1_1_1d git://github.com/openssl/openssl.git thirdparty/openssl
-git clone --branch release-2.1.8-stable git://github.com/libevent/libevent.git thirdparty/libevent
-git clone --branch v1.24.1 git://github.com/libuv/libuv.git thirdparty/libuv
-curl http://dist.schmorp.de/libev/Attic/libev-4.24.tar.gz | tar zx -C thirdparty
