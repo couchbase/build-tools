9d6699b02381d53d8480f161b007b0f8f364a65a
Chris Hillery <ceej@couchbase.com>
2020-06-16 14:53:26 -0700

    Fix k8s-docker job to push all images
    Change-Id: I9893ead9af5212a73b0977a78a108c7b85782f46
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/130660
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- k8s-microservice/jenkins/couchbase-k8s-microservice-docker.sh
+++ k8s-microservice/jenkins/couchbase-k8s-microservice-docker.sh
@@ -12,15 +12,18 @@ chk_set BLD_NUM
 ${script_dir}/util/build-k8s-images.sh ${PRODUCT} ${VERSION} ${BLD_NUM} 1
 
 # Now retag and push to internal and gitlab registries
-short_product=${PRODUCT/couchbase-/}
-tag=${VERSION}-${BLD_NUM}
-for org in cb-vanilla cb-rhcc; do
-    local_org_image=${org}/${short_product}:${tag}
-    for registry in build-docker.couchbase.com registry.gitlab.com; do
-        remote_org_image=${registry}/${org}/${short_product}:${tag}
-        docker tag ${local_org_image} ${remote_org_image}
-        docker push ${remote_org_image}
-        docker rmi ${remote_org_image}
+pushd images
+for product in *; do
+    short_product=${product/couchbase-/}
+    tag=${VERSION}-${BLD_NUM}
+    for org in cb-vanilla cb-rhcc; do
+        local_org_image=${org}/${short_product}:${tag}
+        for registry in build-docker.couchbase.com registry.gitlab.com; do
+            remote_org_image=${registry}/${org}/${short_product}:${tag}
+            docker tag ${local_org_image} ${remote_org_image}
+            docker push ${remote_org_image}
+            docker rmi ${remote_org_image}
+        done
+        docker rmi ${local_org_image}
     done
-    docker rmi ${local_org_image}
 done
