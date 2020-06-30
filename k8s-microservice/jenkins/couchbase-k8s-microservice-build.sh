9b5429ced1fe001ef7fd473e9ed4487c4903a177
Chris Hillery <ceej@couchbase.com>
2020-06-30 01:09:20 -0700

    CBD-3494: Notarize command-line utils for couchbase-operator builds
    Change-Id: If03cf4bb58c79f388d9d50e1acbc12d6bd57eee1
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/131614
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- k8s-microservice/jenkins/couchbase-k8s-microservice-build.sh
+++ k8s-microservice/jenkins/couchbase-k8s-microservice-build.sh
@@ -8,3 +8,28 @@ chk_set PRODUCT
 
 cd ${PRODUCT}
 make dist
+
+if [ "${PRODUCT}" != "couchbase-operator" ]; then
+    exit 0
+fi
+
+# Special case code for couchbase-operator: Notarize the command-line programs
+set +x
+for file in dist/*.zip
+do
+    case "$file" in
+        *"operator"*"macos"*".zip")
+        echo "Signing and notarizing $file"
+        platform=$(echo $file | sed -e "s/.*operator-\([^_]*\).*/\1/g")
+        bundle="com.couchbase.autonomous-operator-${platform}-${VERSION}-${BLD_NUM}"
+        curl -Lsf -o "${file}" \
+                -X POST \
+                -F "notarize=true" \
+                -F "binary_locations=bin" \
+                -F "bundle=${bundle}" \
+                -F "content=@${file}" \
+                -F "token=$(cat /ssh/notarizer_token)" \
+                http://172.23.113.4:7000/zip/${file//dist\//}
+        ;;
+    esac
+done
