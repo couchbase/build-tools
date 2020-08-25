aab6bfbb509d9f64b10e7cc11885284b5388f6a7
udkyo <blair.watt@couchbase.com>
2020-08-25 11:58:05 +0100

    Remove patch_via_gerrit (moved to couchbasedeps/patch_via_gerrit)
    Change-Id: I90eb557db559a8a84face7561a0044ac7a988790
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/134975
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- gerrit-tools/gerrit_tools/scripts/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import os
-from subprocess import Popen, PIPE
-
-def reset_checkout():
-    process = Popen(['repo', 'sync', '-j8'], stdout=PIPE, stderr=PIPE, cwd=os.getenv('source_path'))
-    stdout, stderr = process.communicate()
--- gerrit-tools/gerrit_tools/version.py
+++
@@ -1,2 +0,0 @@
-# Update this file for version changes
-__version__ = '1.0.0'
