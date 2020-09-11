580e4ceba69535920f81391d6f6edff8f44a3202
Ming Ho <ming.ho@couchbase.com>
2020-09-11 11:11:02 -0700

    CBD-3616, exclude LiteCore-iOS due to AloneMonkey/iOSREBook
    -Ming Ho
    
    Change-Id: I3c119b414a92f2c3d03f5dfaa49742285719cd6e
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/136078
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-core/prune_source.sh
+++ blackduck/couchbase-lite-core/prune_source.sh
@@ -2,6 +2,9 @@
 
 pwd
 
+##remove LiteCore-iOS and libstemmer_c per CBD-3616
+rm -rf couchbase-lite-core/Xcode/LiteCore-iOS/LiteCore-iOS
+
 # cleanup unwanted stuff - test files, build scripts, etc.
 for i in fleece Docs doc test tests docs googletest third_party cbbuild tlm libstemmer_c; do
      find . -type d -name $i | xargs rm -rf
