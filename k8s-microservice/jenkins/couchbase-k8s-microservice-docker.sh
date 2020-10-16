df9a41d2459e8d2bdbbe6f8c0f25a0723487f737
Chris Hillery <ceej@couchbase.com>
2020-10-15 19:46:59 -0700

    CBD-3700: Add :latest to Operator image builds when building highest version
    Change-Id: Id1568c4efe11b81ed1626f4777d41a24c45fb5b0
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/138285
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- k8s-microservice/jenkins/couchbase-k8s-microservice-docker.sh
+++ k8s-microservice/jenkins/couchbase-k8s-microservice-docker.sh
@@ -11,18 +11,34 @@ chk_set BLD_NUM
 # First build the images into the local cb-xxxxx organizations
 ${script_dir}/util/build-k8s-images.sh ${PRODUCT} ${VERSION} ${BLD_NUM} 1
 
+# Figure out if this is the highest current version being built
+highest_version=$(
+    curl --silent http://dbapi.build.couchbase.com:8000/v1/products/${PRODUCT}/releases \
+    | jq --raw-output '.releases[-1]'
+)
+
+# Compute tag(s) to push
+version_build=${VERSION}-${BLD_NUM}
+if [ ${highest_version} = ${VERSION} ]; then
+    tags="${version_build} latest"
+else
+    tags="${version_build}"
+fi
+
 # Now retag and push to internal and gitlab registries
 pushd images
 for product in *; do
     short_product=${product/couchbase-/}
-    tag=${VERSION}-${BLD_NUM}
     for org in cb-vanilla cb-rhcc; do
-        local_org_image=${org}/${short_product}:${tag}
+        local_org_image=${org}/${short_product}:${version_build}
         for registry in build-docker.couchbase.com registry.gitlab.com; do
-            remote_org_image=${registry}/${org}/${short_product}:${tag}
-            docker tag ${local_org_image} ${remote_org_image}
-            docker push ${remote_org_image}
-            docker rmi ${remote_org_image}
+            for tag in ${tags}; do
+                remote_org_image=${registry}/${org}/${short_product}:${tag}
+
+                docker tag ${local_org_image} ${remote_org_image}
+                docker push ${remote_org_image}
+                docker rmi ${remote_org_image}
+            done
         done
         docker rmi ${local_org_image}
     done
