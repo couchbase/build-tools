e3d71bb2f284916db95b6877c8ad75d5d09ab507
Chris Hillery <ceej@couchbase.com>
2021-04-25 23:36:04 -0700

    CBD-4058: Black Duck scans for couchbase-operator-backup
    Change-Id: Ifdb6d82719dead9eeaac24e44a8d446e82c63b00
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/152065
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ blackduck/couchbase-operator-backup/get_additional_source.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ex
+
+RELEASE=$1
+
+# Create "bd-venv" in *parent* directory - run-scanner looks for this
+python3 -m venv ../bd-venv
+. ../bd-venv/bin/activate
+pip install -r couchbase-operator-backup/requirements.txt
+
+# Since we're about to run "repo" again, need to kill existing .repo dir
+rm -rf .repo
+
+# We need these repositories from the Server build as well.
+# couchbase-operator-backup RELEASE values always correspond to a released
+# VERSION of couchbase-server.
+mkdir server_src
+cd server_src
+repo init \
+    -u git://github.com/couchbase/manifest \
+    -m released/couchbase-server/${RELEASE}.xml \
+    -g backup
+repo sync --jobs=8
+rm -rf forestdb
+
+# Prior to 7.0, backup was built with GOPATH, so we want to eliminate any
+# "couchbase" packages before doing Black Duck signature scan. In 7.0,
+# backup is built with Go modules, and the 'backup' group in the manifest
+# already prunes out everything we don't need.
+if [[ "${RELEASE}" =~ ^6 ]]; then
+    rm -rf go*/src/*/couchbase*
+    # Also delete any go.mod files - leads to false positives in BD
+    find go* -name go.mod -print0 | xargs -0 rm
+fi
