2da86a256befab8292e129183609e66949fc0513
Chris Hillery <ceej@couchbase.com>
2021-05-04 01:11:57 -0700

    CBD-4078, CBD-4084: Remove unwanted static libs and pkgconfig files.
    Change-Id: If7310e3ab123cfafd7ddd57b935547964c57bfdb
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/152749
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- cbdeps/libuv/libuv_unix.sh
+++ cbdeps/libuv/libuv_unix.sh
@@ -11,6 +11,12 @@ cd ${ROOT_DIR}/libuv
 make
 make install
 
+# Clean up unwanted stuff
+(
+    cd ${INSTALL_DIR}/lib
+    rm -rf pkgconfig libuv.la libuv.a
+)
+
 # For MacOS, tweak install_name
 if [ $(uname -s) = "Darwin" ]; then
     install_name_tool -id @rpath/libuv.1.dylib ${INSTALL_DIR}/lib/libuv.1.dylib
