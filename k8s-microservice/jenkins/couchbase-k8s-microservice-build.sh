242cd77f40dc20f98da85d181090a50f3ae85f7c
Chris Hillery <ceej@couchbase.com>
2021-05-04 00:45:47 -0700

    CBD-4067: Parse manifest using 'xmllint' rather than 'repo'
    Change-Id: I11a74448644f16b68737551e24017053d76361fa
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/152519
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- k8s-microservice/jenkins/couchbase-k8s-microservice-build.sh
+++ k8s-microservice/jenkins/couchbase-k8s-microservice-build.sh
@@ -8,9 +8,14 @@ chk_set PRODUCT
 chk_set VERSION
 chk_set BLD_NUM
 
-GOVERSION=$(repo forall build -c 'echo ${REPO__GOVERSION:-1.13.3}')
-GODIR=${HOME}/golang
-mkdir -p ${GODIR}
+# Extract Golang version to use from manifest
+GOANNOTATION=$(xmllint \
+    --xpath 'string(//project[@name="build"]/annotation[@name="GOVERSION"]/@value)' \
+    manifest.xml)
+GOVERSION=${GOANNOTATION:-1.13.3}
+
+# Create temp directory in WORKSPACE to install golang
+GODIR=$(mktemp -d -q --tmpdir=$(pwd) golangXXXXX)
 cbdep install -d ${GODIR} golang ${GOVERSION}
 export PATH=${GODIR}/go${GOVERSION}/bin:${PATH}
 
