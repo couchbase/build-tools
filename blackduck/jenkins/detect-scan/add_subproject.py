bdaf16eb75704103c5b1fca0b32d3587dcdecbd5
Ming Ho <ming.ho@couchbase.com>
2021-05-10 19:14:34 -0700

    CBD-4092, blackduck sub-project improvement.  Sort all versions of the sub-project.  Use the latest of matching x.y version as sub-project.
    -Ming Ho
    
    Change-Id: I98a0df6194371d11db43a74b5cb1a4109cd70428
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/153311
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/jenkins/detect-scan/add_subproject.py
+++ blackduck/jenkins/detect-scan/add_subproject.py
@@ -42,9 +42,27 @@ if parent_project is None:
     sys.exit(parent_project+' or its version is NOT found.')
 
 #create parent/sub project relationships
-for p in sub_project_list:
-    sub_project = hub.get_project_version_by_name(p, args.version)
+for sub_project_name in sub_project_list:
+    sub_project = hub.get_project_by_name(sub_project_name)
     if sub_project is None:
-        sys.exit(sub-project+' or its version does not exist. It can NOT be added as a sub-project.')
+        sys.exit(sub-project +' does not exist on blackduck. It can NOT be added as a sub-project.')
+    sub_project_versions = hub.get_project_versions(sub_project)
+    if 'totalCount' in sub_project_versions and sub_project_versions['totalCount'] > 0:
+        #Get all version of the sub-project from blackduck
+        #Find the latest of x.y.z and add it as the sub-project
+        #For example:
+        #  couchbase-lite-core has 2.7.4, 2.8.0, 2.8.4, 3.0.0
+        #    2.7.x is mercury
+        #    2.8.x is hydrogen
+        #    3.0.x is lithium
+        #  when adding it as sub-project for CBL android 2.8.5, the version to match is 2.8.
+        #  and 2.8.4 should be picked.
+        versions = sub_project_versions['items']
+        sorted_versions = sorted(versions, key=lambda k: k['versionName'], reverse=True)
+        version_to_match=args.version.rsplit('.', 1)[0]
+        index = next((index for (index, d) in enumerate(sorted_versions) if version_to_match in d['versionName']), None)
+        if index is None:
+            sys.exit("No matching version is found for sub-project: " + sub_project_name)
+        hub.add_version_as_component(parent_project, sorted_versions[index])
     else:
-        hub.add_version_as_component(parent_project, sub_project)
+        sys.exit('No version is found on blackduck for ' + sub_project_name)
