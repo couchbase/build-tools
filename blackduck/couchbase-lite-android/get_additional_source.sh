966f1763db4b91e5a06283f0fe5344c35c8b40f0
Ming Ho <ming.ho@couchbase.com>
2021-05-27 14:54:05 -0700

    CBD-4132, add cmake install for android sdk.
    -Ming Ho
    
    Change-Id: I6b8320be66b1cdbfc39b703ec8ba58bb818b05de
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/154598
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-android/get_additional_source.sh
+++ blackduck/couchbase-lite-android/get_additional_source.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -ex
 NDK_VERSION=$(cat cbl-java/ee/android/etc/jenkins/build.sh | grep ^NDK_VERSION |awk -F "\'|\"" '{print $2}')
+CMAKE_VERSION=$(cat cbl-java/ee/android/etc/jenkins/build.sh | grep ^CMAKE_VERSION |awk -F "\'|\"" '{print $2}')
 SDK_DIR=/home/couchbase/tools/android-sdk
 SDK_MGR=${SDK_DIR}/tools/bin/sdkmanager
 NDK_DIR=${SDK_DIR}/ndk/${NDK_VERSION}
+CMAKE_DIR=${SDK_DIR}/cmake/${CMAKE_VERSION}
 if [ ! -d ${NDK_DIR} ]; then
   ${SDK_MGR} --install "ndk;${NDK_VERSION}"
+fi
+if [ ! -d ${CMAKE_DIR} ]; then
+  ${SDK_MGR} --install "cmake;${CMAKE_VERSION}"
 fi
 if [ ! -f "local.properties" ]; then
     echo "ndk.dir=${NDK_DIR}" > local.properties
     echo "sdk.dir=${SDK_DIR}" >> local.properties
+    echo "cmake.dir=${CMAKE_DIR}" >> local.properties
 fi
 cp local.properties cbl-java/local.properties
 cp local.properties cbl-java/ce/android/local.properties
