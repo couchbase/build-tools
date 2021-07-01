cd370fac3941a3a9219eb320176476c414213d53
Chris Hillery <ceej@couchbase.com>
2021-07-01 03:59:33 -0700

    Don't scan 'build' repo for CBL Net/iOS
    Change-Id: I10420caf4f3b3336dba4a26c2b028de5107c5286
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/156769
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/couchbase-lite-ios/prune_source.sh
+++ blackduck/couchbase-lite-ios/prune_source.sh
@@ -2,6 +2,6 @@
 
 #clean up unnecessary source directories
 #couchbase-lite-core is attached as a sub-project, no need to scan
-for i in fleece gradle Docs doc test tests docs googletest third_party _obsolete VisualStudio dotzlib MYUtilities couchbase-lite-core-EE couchbase-lite-core; do
+for i in cbbuild fleece gradle Docs doc test tests docs googletest third_party _obsolete VisualStudio dotzlib MYUtilities couchbase-lite-core-EE couchbase-lite-core; do
    find . -type d -name $i | xargs rm -rf
 done
