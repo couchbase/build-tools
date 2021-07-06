dc3651c579c90d86c7bb59f2b60c65daf60d6e63
Blair Watt <blair.watt@couchbase.com>
2021-07-06 16:09:39 +0100

    CBD-4180: Ensure breakpad builds on aarch64
    Change-Id: I7baffb9902db5cf5b70cabb5d20864a35b362aad
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/156775
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- cbdeps/breakpad/breakpad_unix.sh
+++ cbdeps/breakpad/breakpad_unix.sh
@@ -5,10 +5,33 @@
 INSTALL_DIR=$1
 ROOT_DIR=$2
 
-cd ${ROOT_DIR}/breakpad
+# Ensure depot_tools is present and pathed
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+pushd depot_tools && git checkout df336feec9a9f43a9239634e763cc7530f5216ca && popd
+export PATH=$(pwd)/depot_tools:$PATH
+
+# gclient sync expects the breakpad source to live in src
+mv breakpad src
+
+# Set up gclient config for tag to pull for breakpad, then do sync
+# (this handles the 'fetch breakpad' done by the usual process)
+cat > .gclient <<EOF
+solutions = [
+  {
+    "url": "https://github.com/couchbasedeps/google-breakpad.git@couchbase-20200430",
+    "managed": False,
+    "name": "breakpad",
+    "deps_file": "DEPS",
+  },
+];
+EOF
+
+export VPYTHON_BYPASS="manually managed python not supported by chrome operations"
+gclient sync
 
 export LDFLAGS="-Wl,-rpath,'\$\$ORIGIN/../lib'"
 
+cd src
 ./configure --prefix=${INSTALL_DIR}
 make
 make install
