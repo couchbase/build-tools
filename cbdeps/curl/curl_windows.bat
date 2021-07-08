36a782115eb7cdb870c8b67969b769325e461d98
Ming Ho <ming.ho@couchbase.com>
2021-07-08 21:22:14 +0000

    Revert "CBD-4183, enable openssl for windows build."
    This reverts commit 84dbf21d22fde97ee013e36981579cd947a1a486.
    
    Reason for revert: Per Isha, she doesn't need libcurl to be compiled with openssl anymore.  CBD-4183 has been closed as won't do.
    
    Change-Id: Iec9a0ea4f74773098eecbe908ddc626f82f6c1a0
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/157189
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- cbdeps/curl/curl_windows.bat
+++ cbdeps/curl/curl_windows.bat
@@ -1,19 +1,9 @@
 set INSTALL_DIR=%1
 set ROOT_DIR=%2
 
-set OPENSSL_VER=1.1.1k-3
-
-set CBDEPS_DIR=%ROOT_DIR%\cbdeps
-
-mkdir %CBDEPS_DIR%
-cd %CBDEPS_DIR%
-
-curl -L https://packages.couchbase.com/cbdep/1.0.1/cbdep-1.0.1-windows.exe -o cbdep.exe || goto error
-cbdep.exe install -d %CBDEPS_DIR% openssl %OPENSSL_VER% || goto error
-
 cd %ROOT_DIR%\curl
 
-set OUTPUT_DIR=builds\libcurl-vc15-x64-release-dll-ssl-dll-ipv6-sspi
+set OUTPUT_DIR=builds\libcurl-vc15-x64-release-dll-ipv6-sspi-winssl
 
 rem Fix that wasn't in 7.66.0 - probably needs to be removed
 rem when building later versions
@@ -21,7 +11,7 @@ git cherry-pick a765a305 || goto error
 
 call .\buildconf.bat || goto error
 cd winbuild
-call nmake -f Makefile.vc mode=dll VC=15 MACHINE=x64 DEBUG=no GEN_PDB=yes WITH_SSL=dll WITH_DEVEL=%CBDEPS_DIR%/openssl-%OPENSSL_VER%|| goto error
+call nmake -f Makefile.vc mode=dll VC=15 MACHINE=x64 DEBUG=no GEN_PDB=yes || goto error
 
 cd ..
 xcopy %OUTPUT_DIR% %INSTALL_DIR% /s || goto error
