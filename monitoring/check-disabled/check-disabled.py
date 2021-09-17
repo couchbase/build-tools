c5704d9635ae1bcf855a1fd482738e7e6542779a
Chris Hillery <ceej@couchbase.com>
2021-09-17 16:20:29 -0700

    Ignore timeouts in warn-if-jobs-disabled job
    The script occasionally blocks reading server.jenkins for whatever
    reason, and times out after two minutes, sending us email. We don't
    really care since the job will run again in 15 minutes, so set an
    explicit 20-second timeout and then catch and ignore it.
    
    Change-Id: I61a860875e887d3518b181b326bda09e899a4058
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/161790
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- monitoring/check-disabled/check-disabled.py
+++ monitoring/check-disabled/check-disabled.py
@@ -5,13 +5,15 @@ import sys
 import json
 import os
 import argparse
+import socket
+from urllib.error import URLError
 from urllib.request import Request, urlopen
 
 parser = argparse.ArgumentParser()
 parser.add_argument("jenkins", type=str, help="Jenkins to connect to",
     nargs='?', default="server.jenkins.couchbase.com")
-parser.add_argument("--job", type=str, help="Mount local directories",
-    nargs="+")
+parser.add_argument("--job", type=str, help="Job name(s)",
+    nargs="+", required=True)
 args = parser.parse_args()
 
 jenkins = args.jenkins
@@ -29,7 +31,18 @@ for job in jobs:
     auth_arg = '%s:%s' % (os.environ.get('jenkins_user'), os.environ.get('jenkins_token'))
     base64string = base64.b64encode(auth_arg.encode())
     request.add_header("Authorization", "Basic %s" % base64string.decode())
-    response = urlopen(request)
+    try:
+        response = urlopen(request, timeout=20)
+    except URLError as e:
+        # We get occasional timeouts, which we'd rather not hear about
+        # since this script will run again in 15 minutes anyway. By setting
+        # timeout=20 above, urllib will abort with a socket.timeout after
+        # 20 seconds, so catch that here.
+        if isinstance(e.reason, socket.timeout):
+            print("Got a timeout; ignoring")
+            continue
+        else:
+            raise
 
     jobdata = json.load(response)
 
