76c0a5f7bdd2c8a4d77980a5c8c66b5fbe710d7e
Chris Hillery <ceej@couchbase.com>
2021-10-06 01:57:46 -0700

    CBD-4359: Build OpenSSL with static libs on Windows
    Change-Id: I971402ef6e2292a9b3a7949a5b7111a465b13eee
    Reviewed-on: http://review.couchbase.org/c/build-tools/+/163063
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- cbdeps/openssl/openssl_windows.bat
+++ cbdeps/openssl/openssl_windows.bat
@@ -1,5 +1,6 @@
 set INSTALL_DIR=%1
 set ROOT_DIR=%2
+set PROFILE=%4
 set ARCH=%8
 
 cd %ROOT_DIR%\openssl
@@ -18,7 +19,19 @@ call nmake || goto error
 call nmake install || goto error
 call xcopy /IE %CD%\build %INSTALL_DIR% || goto error
 call rmdir /s /q %INSTALL_DIR%\html || goto error
-goto :eof
+
+set STATIC_DIR=%INSTALL_DIR%\lib\VC\static
+if "%PROFILE%" == "server" (
+    rem Erlang wants static openssl libs, and has some specific ideas about
+    rem where they should be and what they should be named. Since nobody
+    rem else cares, go along with that. Put them in a top-level "staticlib"
+    rem subdirectory so they don't get installed as part of the Server
+    rem build.
+    call mkdir %STATIC_DIR% || goto error
+    call copy libcrypto_static.lib %STATIC_DIR%\libcryptoMD.lib || goto error
+    call copy libssl_static.lib %STATIC_DIR%\libsslMD.lib || goto error
+    goto :eof
+)
 
 :error
 echo Failed with error %ERRORLEVEL%
