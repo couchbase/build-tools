9fc3dd5409e91966f498171e2daeaa101a3c077e
Chris Hillery <ceej@couchbase.com>
2022-01-20 19:32:50 -0800

    CBL-2487: Improve cbl-core scans by pruning less
    Can't remove "docs" and "tests" directories, or else Black Duck fails
    to identify both mbedtls and SQLiteCpp.
    
    Change-Id: I97a65f6f1db4901f9fd1bcebdc60124aa50a5e92
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/169189
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/couchbase-lite-core/prune_source.sh
+++ blackduck/couchbase-lite-core/prune_source.sh
@@ -7,6 +7,6 @@ rm -rf couchbase-lite-core/Xcode/LiteCore-iOS/LiteCore-iOS
 
 # cleanup unwanted stuff - test files, build scripts, etc.
 # also remove first-party code such as fleece.
-for i in fleece Docs doc test tests docs googletest third_party cbbuild tlm libstemmer_c; do
+for i in fleece Docs doc test googletest third_party cbbuild tlm libstemmer_c; do
      find . -type d -name $i | xargs rm -rf
 done
