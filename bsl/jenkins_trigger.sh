559c8616c69cadccd14d9ee798e57c1a5b29297c
Chris Hillery <ceej@couchbase.com>
2022-03-02 23:07:36 -0800

    Improvements to BSL scripts
    - Add -l to run license change only (no update to header comments)
    - Add -p to run only for specified projects (default is all 'bsl')
    - Clean out any existing license files before creating ours
    - Limit -f force option to only overwriting VERSION, not PRODUCT in
      projects with existing BSL license files
    - Sync manifest more intelligently (only bsl group; clean up local
      changes before running checks)
    - Clean up job output
    
    Change-Id: I934c95e7b75c60d32b6bcea9c148a590c99385ca
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/171626
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- bsl/jenkins_trigger.sh
+++ bsl/jenkins_trigger.sh
@@ -1,17 +1,28 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 NODE_VERSION=17.1.0
 
-[ ! -f "./cbdep" ] || ! ./cbdep --version &>/dev/null \
-    && echo "Downloading cbdep" \
-    && curl http://downloads.build.couchbase.com/cbdep/cbdep.linux -o ./cbdep && chmod 755 ./cbdep
+# Set script args based on env vars from Jenkins parameters
+${DRY_RUN} && export DRY_RUN_ARG=-n
+${FORCE} && export FORCE_ARG=-f
+${LICENSES_ONLY} && export LICENSES_ONLY_ARG=-l
+PROJECTS_ARG="-p ${PROJECTS:-bsl}"
 
-[ ! -f "./nodejs-${NODE_VERSION}/bin/node" ] \
-    && echo "Installing node" \
-    && ./cbdep install -d . nodejs ${NODE_VERSION}
+# We don't need nodejs if we're not injecting header comments
+${LICENSES_ONLY} || {
 
-export PATH=$(pwd)/nodejs-${NODE_VERSION}/bin:$PATH
+    [ ! -f "./cbdep" ] || ! ./cbdep --version &>/dev/null \
+        && echo "Downloading cbdep" \
+        && curl http://downloads.build.couchbase.com/cbdep/cbdep.linux -o ./cbdep && chmod 755 ./cbdep
 
-./update-bsl-for-manifest ${DRY_RUN_ARG} ${FORCE_ARG} \
+    [ ! -f "./nodejs-${NODE_VERSION}/bin/node" ] \
+        && echo "Installing node" \
+        && ./cbdep install -d . nodejs ${NODE_VERSION}
+
+    export PATH=$(pwd)/nodejs-${NODE_VERSION}/bin:$PATH
+}
+
+./update-bsl-for-manifest \
+    ${DRY_RUN_ARG} ${FORCE_ARG} ${LICENSES_ONLY_ARG} ${PROJECTS_ARG} \
     -u "${MANIFEST_REPO}" \
     -m "${MANIFEST}"
