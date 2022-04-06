a37c1ea95c782caf98701fcb4bfec2e2cf246b99
Ming Ho <ming.ho@couchbase.com>
2022-04-06 11:57:37 -0700

    CBD-4730, fixed BD scan failure for CBL android add openjdk8 installation so that sdkmanager can be launched properly use cbdep to install cmake instead of sdkmanager.
    -Ming Ho
    
    Change-Id: I77d008ea0003e827d316ee39ce68e3d8bead2aae
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/173377
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-android/get_additional_source.sh
+++ blackduck/couchbase-lite-android/get_additional_source.sh
@@ -1,16 +1,28 @@
 #!/bin/bash -ex
 NDK_VERSION=$(cat cbl-java/ee/android/etc/jenkins/build.sh | grep ^NDK_VERSION |awk -F "\'|\"" '{print $2}')
 CMAKE_VERSION=$(cat cbl-java/ee/android/etc/jenkins/build.sh | grep ^CMAKE_VERSION |awk -F "\'|\"" '{print $2}')
-SDK_DIR=/home/couchbase/tools/android-sdk
+TOOLS_DIR=/home/couchbase/tools
+SDK_DIR=${TOOLS_DIR}/android-sdk
 SDK_MGR=${SDK_DIR}/tools/bin/sdkmanager
 NDK_DIR=${SDK_DIR}/ndk/${NDK_VERSION}
-CMAKE_DIR=${SDK_DIR}/cmake/${CMAKE_VERSION}
+CMAKE_DIR=${TOOLS_DIR}/cmake-${CMAKE_VERSION}
+#sdkmanager doesn't work with jdk11, we have to install jdk8 here.
+OPEN_JDK_VERSION=8u292-b10
+curl https://packages.couchbase.com/cbdep/cbdep-linux -o cbdep
+chmod +x cbdep
+if [ ! -d ${TOOLS_DIR}/openjdk-${OPENJDK_VERSION} ]; then
+  ./cbdep install -d ${TOOLS_DIR} openjdk ${OPEN_JDK_VERSION}
+fi
+JAVA_HOME=${TOOLS_DIR}/openjdk-${OPENJDK_VERSION}
+
 if [ ! -d ${NDK_DIR} ]; then
   ${SDK_MGR} --install "ndk;${NDK_VERSION}"
 fi
 if [ ! -d ${CMAKE_DIR} ]; then
-  ${SDK_MGR} --install "cmake;${CMAKE_VERSION}"
+  ./cbdep install -d ${TOOLS_DIR} cmake ${CMAKE_VERSION}
 fi
+unset JAVA_HOME
+
 if [ ! -f "local.properties" ]; then
     echo "ndk.dir=${NDK_DIR}" > local.properties
     echo "sdk.dir=${SDK_DIR}" >> local.properties
