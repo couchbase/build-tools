c9c6714c45842ad73c4eedcb277b6c26b1c4fee4
Sergey Avseyev <sergey.avseyev@gmail.com>
2022-04-25 15:10:57 +0300

    PCBC-838: Add new 4.0 version to Blackduck scans
    Change-Id: I8e49ac0a6926b4c497cede1b7ce2d4745f210121
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/174130
    Reviewed-by: Brett Lawson <brett19@gmail.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

--- blackduck/couchbase-sdk-php/get_source.sh
+++ blackduck/couchbase-sdk-php/get_source.sh
@@ -5,9 +5,20 @@ RELEASE=$2
 VERSION=$3
 BLD_NUM=$4
 
-git clone ssh://git@github.com/couchbase/php-couchbase.git
-pushd php-couchbase
-TAG="v$VERSION"
+case "$VERSION" in
+    3.1*|3.2*)
+        REPO=ssh://git@github.com/couchbase/php-couchbase.git
+        VERSION_PREFIX=v
+        ;;
+    *)
+        REPO=ssh://git@github.com/couchbase/couchbase-php-client.git
+        VERSION_PREFIX=
+        ;;
+esac
+
+git clone --recurse-submodules $REPO couchbase-php-client
+pushd couchbase-php-client
+TAG="${VERSION_PREFIX}${VERSION}"
 if git rev-parse --verify --quiet $TAG >& /dev/null
 then
     echo "Tag $TAG exists, checking it out"
