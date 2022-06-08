c7e01c5c74c02e15271332cc6d790af8842fb4bf
Ming Ho <ming.ho@couchbase.com>
2022-06-08 10:21:31 -0700

    MB-52314, build scripts for libsodium.
    -Ming Ho
    
    Change-Id: I50f40b58eab55490af0c1774071179d01c19ceb1
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/175784
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

---
+++ cbdeps/libsodium/libsodium_unix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+INSTALL_DIR=$1
+ROOT_DIR=$2
+
+cd ${ROOT_DIR}/libsodium
+./configure --prefix=${INSTALL_DIR}
+make && make check
+make install
+
+# For MacOS, tweak install_name
+if [ $(uname -s) = "Darwin" ]; then
+    install_name_tool -id @rpath/libsodium.dylib ${INSTALL_DIR}/lib/libsodium.dylib
+fi
+
+#rm -rf ${INSTALL_DIR}/lib/pkgconfig
---
+++ cbdeps/libsodium/libsodium_windows.bat
@@ -0,0 +1,36 @@
+@echo on
+
+set INSTALL_DIR=%1
+set ROOT_DIR=%2
+
+cd %ROOT_DIR%\libsodium
+
+
+pushd builds\msvc\build
+rem buildbase.bat uses Enterprise or Community version of MSVS to do builds
+rem We have Professional version.  Replace Community with Professional before running it.
+setlocal enabledelayedexpansion
+powershell -Command "(gc buildbase.bat) -replace 'Community','Professional' | Set-Content buildbase.bat"
+
+
+rem Build libsodium libraries
+call buildbase.bat ..\vs2017\libsodium.sln 15 || goto error
+
+popd
+
+rem Copy right stuff to output directory.
+mkdir %INSTALL_DIR%\lib
+mkdir %INSTALL_DIR%\include\sodium
+
+copy bin\Win32\Release\v141\dynamic\libsodium.lib %INSTALL_DIR%\lib || goto error
+copy bin\Win32\Release\v141\dynamic\libsodium.pdb %INSTALL_DIR%\lib || goto error
+copy bin\Win32\Release\v141\dynamic\libsodium.dll %INSTALL_DIR%\bin || goto error
+
+copy src\libsodium\include\sodium.h %INSTALL_DIR%\include || goto error
+copy src\libsodium\include\sodium\*.h %INSTALL_DIR%\include\sodium || goto error
+
+goto :eof
+
+:error
+echo Failed with error %ERRORLEVEL%
+exit /B %ERRORLEVEL%
