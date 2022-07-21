ff4c60645d17be4b4538a4d5f6bcc3130875ec7f
Ming Ho <ming.ho@couchbase.com>
2022-07-21 15:35:18 -0700

    CBD-4918, per discussion with Lynn, add direct-nebula and couchbase-data-api as part of couchbase-cloud to blackduck.
    -Ming Ho
    
    Change-Id: I97d9b3ecb1f4c0300ee361a496ac72fcf1a76b50
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/177880
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-cloud/get_source.sh
+++ blackduck/couchbase-cloud/get_source.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -ex
-
 git clone ssh://git@github.com/couchbasecloud/couchbase-cloud.git
+git clone ssh://git@github.com/couchbase/direct-nebula.git
+git clone ssh://git@github.com/couchbaselabs/data-api.git
+
+for repo in couchbase-cloud direct-nebula data-api; do
+    pushd ${repo}
+    go mod download
+    popd
+done
+
+echo "replace github.com/couchbasecloud/couchbase-cloud => ../couchbase-cloud" >> direct-nebula/go.mod
+echo "replace github.com/couchbasecloud/couchbase-cloud => ../couchbase-cloud" >> data-api/go.mod
