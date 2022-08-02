e9fd7db1b0ceabde931bd294bc4daeb687b29473
Ming Ho <ming.ho@couchbase.com>
2022-08-02 16:16:31 -0700

    remove couchbase-data-api and direct-nebula scripts from build-tools per Aaron's request. scripts are moved to a private repo, cloud-build-tools.
    -Ming Ho
    
    Change-Id: I1bc8e9b318af7da6454d9bfaab82dfe152805137
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/178362
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- couchbase-cloud/couchbase-data-api/iptables-firewall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-iptables -A OUTPUT -d 169.254.169.254 -m owner --uid-owner dataapi -j DROP
--- couchbase-cloud/direct-nebula/iptables-firewall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-iptables -A OUTPUT -d 169.254.169.254 -m owner --uid-owner nebula -j DROP
