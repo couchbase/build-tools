87bb7a0a0831bdafa2349972980d7e293919d164
Chris Hillery <ceej@couchbase.com>
2022-08-04 00:23:24 -0700

    MB-53249: No need to download cbdep in prometheus_unix anymore
    Change-Id: Ib38904e2914ca785d288db9e55631313fcce6b64
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/178454
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- cbdeps/prometheus/prometheus_unix.sh
+++ cbdeps/prometheus/prometheus_unix.sh
@@ -13,18 +13,6 @@ DEPS=${WORKSPACE}/deps
 rm -rf ${DEPS}
 NODEJS_VER=16.5.0
 
-# Download cbdep, unless it's already available in the local .cbdepscache
-OPSYS=$(uname -s | tr "[:upper:]" "[:lower:]")
-CBDEP_BIN_CACHE=/home/couchbase/.cbdepscache/cbdep/${CBDEP_TOOL_VER}/cbdep-${CBDEP_TOOL_VER}-${OPSYS}
-
-if [[ -f ${CBDEP_BIN_CACHE} ]]; then
-    cp ${CBDEP_BIN_CACHE} /tmp/cbdep
-else
-    # Can't trust ${ARCH} here - might be cross-compiling. Use uname -m.
-    CBDEP_URL=https://packages.couchbase.com/cbdep/${CBDEP_TOOL_VER}/cbdep-${CBDEP_TOOL_VER}-${OPSYS}-$(uname -m)
-    curl -o /tmp/cbdep ${CBDEP_URL}
-fi
-
 # Extract GOVERSION from manifest, and install using cbdep
 GO_VER=$(gover_from_manifest)
 cbdep install -d ${DEPS} golang ${GO_VER}
