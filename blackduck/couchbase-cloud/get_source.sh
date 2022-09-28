8d399bea1a8682605bc90ef19cfc36f1ba3add22
Ming Ho <ming.ho@couchbase.com>
2022-09-28 11:14:00 -0700

    CBD-5145 fixes BD scan failures for direct-nebula and couchbase-data-api * moved "go mod download" after replace directives are set in go.mod * add GONOSUMDB for node_exporter due to checksum failure.  This can be removed later after node_exporter fixes there problem.
        -Ming Ho
    
    Change-Id: I422c869dbcac77b6d30f8c9cdd78758e17001570
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/180559
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-cloud/get_source.sh
+++ blackduck/couchbase-cloud/get_source.sh
@@ -4,13 +4,13 @@ git clone ssh://git@github.com/couchbase/direct-nebula.git
 git clone ssh://git@github.com/couchbase/data-api.git
 git clone ssh://git@github.com/couchbase/regulator.git ../extra/regulator
 
+echo "replace github.com/couchbasecloud/couchbase-cloud => ../couchbase-cloud" >> direct-nebula/go.mod
+echo "replace github.com/couchbasecloud/couchbase-cloud => ../couchbase-cloud" >> data-api/go.mod
+echo "replace github.com/couchbase/regulator => ../../extra/regulator" >> data-api/go.mod
+echo "replace github.com/couchbase/regulator => ../../extra/regulator" >> direct-nebula/go.mod
+
 for repo in couchbase-cloud direct-nebula data-api; do
     pushd ${repo}
     go mod download
     popd
 done
-
-echo "replace github.com/couchbasecloud/couchbase-cloud => ../couchbase-cloud" >> direct-nebula/go.mod
-echo "replace github.com/couchbasecloud/couchbase-cloud => ../couchbase-cloud" >> data-api/go.mod
-echo "replace github.com/couchbase/regulator => ../../extra/regulator" >> data-api/go.mod
-echo "replace github.com/couchbase/regulator => ../../extra/regulator" >> direct-nebula/go.mod
---
+++ blackduck/couchbase-cloud/scan-environment.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+cat <<EOF
+export GONOSUMDB="github.com/prometheus/node_exporter"
+EOF
