046a47ea54fe42b9556cb2c1b68e24e92fffd56d
Ming Ho <ming.ho@couchbase.com>
2022-10-31 09:42:10 -0700

    CBD-5013, BD scanning scripts for Spark connector.
    -Ming Ho
    
    Change-Id: I04dccd0211ace25593e324c16c078e5797fd4538
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/182054
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Well-Formed: Restriction Checker
    Tested-by: Ming Ho <ming.ho@couchbase.com>

---
+++ blackduck/couchbase-connector-spark/get_source.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+
+PRODUCT=$1
+RELEASE=$2
+VERSION=$3
+BLD_NUM=$4
+
+git clone ssh://git@github.com/couchbase/couchbase-spark-connector.git
+
+pushd couchbase-spark-connector
+if git rev-parse --verify --quiet $VERSION >& /dev/null
+then
+    echo "Tag $VERSION exists, checking it out"
+    git checkout $VERSION
+else
+    echo "No tag $VERSION, assuming master"
+fi
+
+# don't need to scan examples or docs
+rm -rf src/test
+rm -rf docs
+
+# Install sbt
+SBT_VERSION=$(cat project/build.properties |grep "sbt.version" |awk -F '=' '{print $2}' |tr -d '[:space:]')
+wget https://github.com/sbt/sbt/releases/download/v${SBT_VERSION}/sbt-${SBT_VERSION}.tgz -O - |tar -xz -C ../../extra
+
+# Add dependency graph plugin required by blackduck
+echo '' >> project/plugins.sbt
+echo 'addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.10.0-RC1")' >> project/plugins.sbt
+
+# Blackduck doesn't support buildless scan of scala project as of 7.0.0
+# Hence, we need to build it before passing the project through the scanner.
+../../extra/sbt/bin/sbt clean compile
+
+popd
