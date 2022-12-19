36679b91b420ef7159895a80e63f2684c3bf78ac
Ming Ho <ming.ho@couchbase.com>
2022-12-19 11:12:07 -0800

    CBD-5233 remove unnecessary test directories from couchbase-lite-c's BD scan.
    -Ming Ho
    
    Change-Id: Ia576141b269c714f1174d6a718cc8cb2aacd4fe6
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/184258
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-lite-c/prune_source.sh
+++ blackduck/couchbase-lite-c/prune_source.sh
@@ -5,8 +5,8 @@ pwd
 # According to readme in rust directory.  It is not shipped.
 rm -rf couchbase-lite-c/bindings/rust
 
-# clean up unnecessary source directories
+# clean up unnecessary source and test directories
 # couchbase-lite-core is attached as a sub-project, no need to scan
-for i in cbbuild tlm couchbase-lite-core-EE couchbase-lite-core; do
+for i in cbbuild tlm couchbase-lite-core-EE couchbase-lite-core test; do
    find . -type d -name $i | xargs rm -rf
 done
