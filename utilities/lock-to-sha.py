95d84217641a9fd3061821cc144393b9ef5cc2fb
Chris Hillery <ceej@couchbase.com>
2023-05-12 15:16:49 -0700

    lock-to-sha.py improvements
    - Add --lock-version-branches option to force-lock git branches that
      have the same name as VERSION (normally left pointing to that branch)
    - Add debug logging including disposition of each project
    
    Change-Id: I6654ac4195d3cf7ff608ff61edb5049c88e7b72c
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/190934
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>

--- utilities/lock-to-sha.py
+++ utilities/lock-to-sha.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import argparse
 import re
 import sys
-import argparse
+import logging
 from lxml import etree
 
 
@@ -44,7 +45,6 @@ def main(args):
     if version_element is not None:
         product_version = version_element.get("value", product_version)
 
-
     # Loop through input_src file
     # Replace the git SHA from src_lock_input xml
     result_dict = tree.iterfind("project")
@@ -57,13 +57,19 @@ def main(args):
         revision = result.get('revision', default_revision)
         # If project is already locked to a SHA, skip it
         if sha_regex.match(revision):
+            logging.debug(f"Project {project} already locked to SHA")
             continue
         # If project is already pointing to a tag, skip it
         if revision.startswith("refs/tags/"):
+            logging.debug(f"Project {project} locked to tag")
             continue
         # If project is already pointing to a branch with the same
-        # name as the manifest's VERSION annotation, skip it
-        if revision == product_version:
+        # name as the manifest's VERSION annotation, skip it (unless told
+        # not to skip it)
+        if revision == product_version and not args.lock_version_branches:
+            logging.debug(
+                f"Project {project} already locked "
+                f"to version branch '{product_version}'")
             continue
         # If master-only is specified and project isn't on master/main,
         # skip it. Note: we don't care if the manifest specifies a
@@ -71,32 +77,33 @@ def main(args):
         # git's normal default branch. So we don't compare against
         # default_revision here.
         if master_only and revision != "master" and revision != "main":
+            logging.debug(f"Project {project} on non-master branch {revision}")
             continue
         # If project was explicitly skipped, skip it
         if project in args.skip_projects:
+            logging.debug(f"Project {project} skipped due to user request")
             continue
         # If specific projects were specified and this isn't one of them,
         # skip it
         if args.projects is not None and project not in args.projects:
+            logging.debug(f"Project {project} not on list of explicit projects")
             continue
         try:
             sha = sha_src_dict[path]['revision']
             result.attrib['revision'] = sha
             updated_projects += 1
-            print(f"Locking {project} to {sha}")
+            logging.info(f"Locking {project} to {sha}")
         except KeyError as e:
-            print(f"Error: {e} {project} not found in \"{args.sha_src}\" input file!")
-            print()
+            logging.fatal(f"Error: {e} {project} not found in \"{args.sha_src}\" input file!")
             sys.exit(1)
 
     # write data
     tree.write(args.output, encoding='UTF-8',
                xml_declaration=True, pretty_print=True)
-    print(f"\n{updated_projects} projects locked")
-    print(f"Output manifest has been generated here: {args.output}")
+    logging.info(f"{updated_projects} projects locked")
+    logging.info(f"Output manifest has been generated here: {args.output}")
     if updated_projects == 0:
-        print("\n     WARNING: manifest unchanged!")
-    print()
+        logging.warning("manifest unchanged!")
 
 
 if __name__ == "__main__":
@@ -138,6 +145,24 @@ if __name__ == "__main__":
         metavar='project',
         help='List of projects to lock (default all non-skipped projects)'
     )
+    parser.add_argument(
+        '--lock-version-branches',
+        action='store_true',
+        help='Also lock projects that are pointing to VERSION-specific git branches'
+    )
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        help='Enable debugging output'
+    )
 
     args = parser.parse_args()
+
+    # Initialize logging
+    logging.basicConfig(
+        stream=sys.stderr,
+        format='%(asctime)s: %(levelname)s: %(message)s',
+        level=logging.DEBUG if args.debug else logging.INFO
+    )
+
     main(args)
