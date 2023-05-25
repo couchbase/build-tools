eaf4d0e34445052331efb068d36209732a9aca58
Chris Hillery <ceej@couchbase.com>
2023-05-25 16:47:23 -0700

    Support variable Alpine versions for make-alpine-golang.sh
    Change-Id: I1edc62cec361fe30f1ead5bb85269d7c53dae37c
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/191620
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- cbdeps/golang/make-alpine-golang.sh
+++ cbdeps/golang/make-alpine-golang.sh
@@ -4,9 +4,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${SCRIPT_DIR}/../../utilities/shell-utils.sh"
 
 GOVER=$1
+ALPINEVER=$2
 chk_set GOVER
+chk_set ALPINEVER
 
-GOLANG_IMAGE=golang:${GOVER}-alpine3.14
+GOLANG_IMAGE=golang:${GOVER}-alpine${ALPINEVER}
 docker pull ${GOLANG_IMAGE}
 docker rm -f gopuller
 docker run -d --name gopuller ${GOLANG_IMAGE} sleep 10000
