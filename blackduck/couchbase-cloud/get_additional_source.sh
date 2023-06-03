3a1c8bae63ceab9c704aded1fbb2f3f129cb6587
Chris Hillery <ceej@couchbase.com>
2023-06-02 23:49:40 -0700

    Fix couchbase-cloud blackduck scan
    cp-jungle-jim has been removed
    
    Change-Id: Icfd18fd293541db092d22fa90bf88550c1b1fe26
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/192039
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/couchbase-cloud/get_additional_source.sh
+++ blackduck/couchbase-cloud/get_additional_source.sh
@@ -7,8 +7,7 @@ cbdep install -d ${WORKSPACE}/extra nodejs ${NODE_VERSION}
 export PATH=${WORKSPACE}/extra/nodejs-${NODE_VERSION}/bin:$PATH
 
 for dir in couchbase-cloud/scripts/insomnia-plugin-cb-token-auth \
-           couchbase-cloud/clientSDKSamples/javascript \
-           couchbase-cloud/cmd/cp-jungle-jim/go-playground
+           couchbase-cloud/clientSDKSamples/javascript
 do
     pushd $dir
     npm install
