34668a49533a25b95446e11d9565f7e3bbf4b02a
Ming Ho <ming.ho@couchbase.com>
2023-06-15 14:38:23 -0700

    CBD-5493, need to install openjdk17 for couchbase-lite-android.
    -Ming
    
    Change-Id: I5e5bb49a1aea76cb349415d0dcdf919b68fc9486
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/192689
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/couchbase-lite-android/get_additional_source.sh
+++ blackduck/couchbase-lite-android/get_additional_source.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -ex
+
+function install_openjdk
+{
+    OPEN_JDK_VERSION="$1"
+    if [ ! -d ${TOOLS_DIR}/openjdk-${OPENJDK_VERSION} ]; then
+        cbdep install -d ${TOOLS_DIR} openjdk ${OPEN_JDK_VERSION}
+    fi
+}
+
+# Main
 if [ -f "cbl-java/etc/jenkins/install_android_toolchain.sh" ]; then
     toolchain_script="cbl-java/etc/jenkins/install_android_toolchain.sh"
 elif [ -f "cbl-java/ee/android/etc/jenkins/build.sh" ]; then
@@ -22,20 +32,25 @@ SDK_DIR=${TOOLS_DIR}/android-sdk
 SDK_MGR=${SDK_DIR}/tools/bin/sdkmanager
 NDK_DIR=${SDK_DIR}/ndk/${NDK_VERSION}
 CMAKE_DIR=${TOOLS_DIR}/cmake-${CMAKE_VERSION}
+
 #sdkmanager doesn't work with jdk11, we have to install jdk8 here.
-OPEN_JDK_VERSION=8u292-b10
-if [ ! -d ${TOOLS_DIR}/openjdk-${OPENJDK_VERSION} ]; then
-  cbdep install -d ${TOOLS_DIR} openjdk ${OPEN_JDK_VERSION}
-fi
+OPENJDK_VERSION=8u292-b10
+install_openjdk ${OPENJDK_VERSION}
 JAVA_HOME=${TOOLS_DIR}/openjdk-${OPENJDK_VERSION}
 
 if [ ! -d ${NDK_DIR} ]; then
   ${SDK_MGR} --install "ndk;${NDK_VERSION}"
 fi
+unset JAVA_HOME
+
+# Gradle needs JDK17.
+OPENJDK_VERSION=17.0.7+7
+install_openjdk ${OPENJDK_VERSION}
+echo "org.gradle.java.home=${TOOLS_DIR}/openjdk-${OPENJDK_VERSION}" >> cbl-java/ee/android/gradle.properties
+
 if [ ! -d ${CMAKE_DIR} ]; then
   cbdep install -d ${TOOLS_DIR} cmake ${CMAKE_VERSION}
 fi
-unset JAVA_HOME
 
 if [ ! -f "local.properties" ]; then
     echo "ndk.dir=${NDK_DIR}" > local.properties
