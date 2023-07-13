e3234abc2445beb9692a54a2c641490e8c90d189
Chris Hillery <ceej@couchbase.com>
2023-07-13 15:25:19 -0700

    Don't scan go.mod files for SGW < 3.1.0
    This was leading to false positives in BD scans
    
    Change-Id: I25cff2c6930849be2e5488e8ca6865c5a7220ab0
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/193831
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/sync_gateway/prune_source.sh
+++ blackduck/sync_gateway/prune_source.sh
@@ -1,21 +1,32 @@
 #!/bin/bash -ex
 
-#clean up unnecessary source directories
+RELEASE=$1
+VERSION=$2
+BLD_NUM=$3
+TOOLS_DIR=$4
 
-# Remove un-needed ns_server got pull in for to use <!-- gozip tools --> purpose
+# clean up unnecessary source directories
+
+# Remove un-needed ns_server git pull in for to use <!-- gozip tools --> purpose
 rm -rf godeps/src/github.com/couchbase/ns_server
 # Remove jQuery from scan CBD-2977
 rm -rf godeps/src/github.com/couchbase/cbgt/rest/static/lib/jquery
 
-#Remove beorn7 and golang_protobuf_extensions from scan CBD-3616
+# Remove beorn7 and golang_protobuf_extensions from scan CBD-3616
 rm -rf godeps/src/github.com/beorn7
 rm -rf godeps/src/github.com/matttproud/golang_protobuf_extensions
 
 # nothing in the build repo should affect third-party reports
 rm -rf cbbuild
 
-#MB-43341, remove couchbase CB components from blackduck report
-#use *couchbase*/$i to ensure these are from couchbase
+# SGW wasn't built with Go modules prior to 3.1, so we shouldn't look at
+# any go.mod files
+if [ "3.1.0" != $(printf "3.1.0\n${VERSION}" | sort -n | head -1) ]; then
+  find . -type f -name go.\?\?\? -delete
+fi
+
+# MB-43341, remove couchbase CB components from blackduck report
+# use *couchbase*/$i to ensure these are from couchbase
 
 for i in cbauth cbgt go-couchbase go-blip gocbconnstr gocb gocbcore gocbconnstr gomemcached goutils
 do
