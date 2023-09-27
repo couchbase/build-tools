8b7eb6caa95cfd3b9ef3e1ff31dc1542cd95902c
Chris Hillery <ceej@couchbase.com>
2023-09-26 18:37:34 -0700

    Enhancements for s3 upload scripts to support more products
    upload_to_s3.sh:
    - CE/EE split ignored by default except for Server (all files uploaded)
    - Uploading "platforms" is generally skipped except for Server
    - For Server >= 7.2.2, only upload "linux macos windows" artifacts
    
    produce_s3_urls_report:
    - Add --editions option to S3 report to split URLs by CE/EE (enabled by
      default for Server); otherwise all files will be grouped together
    - Add --staging option to s3 report (mostly for testing)
    - Staging URLs will use http: since packages-staging doesn't have
      correct SSL certificate
    
    Change-Id: Ic4ab5c915765bfce858a60a8a9597faab84c3288
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/197793
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- release/s3/upload_to_s3.sh
+++ release/s3/upload_to_s3.sh
@@ -2,18 +2,18 @@
 
 function usage() {
     echo
-    echo "$0 -r <release-code-name> -v <version-number> -b <build-number>"
+    echo "$0 -r <release> -v <version> -b <build-number>"
     echo "   [-t <product>] [-s <suffix> ] [-c <private | public | only>]"
     echo "   [-p <platforms>] [-l]"
     echo "where:"
-    echo "  -r: release code name; mad-hatter; cheshire-cat; etc."
+    echo "  -r: release code name (defaults to <version>)"
     echo "  -v: version number; eg. 7.0.0"
     echo "  -b: build number to release"
     echo "  -t: product; defaults to couchbase-server"
     echo "  -s: version suffix, eg. 'MP1' or 'beta' [optional]"
     echo "  -c: how to handle CE builds [optional]. Legal values are:"
-    echo "        private: to make it non-downloadable (default)"
-    echo "        public: CE builds are downloadable"
+    echo "        private: to make it non-downloadable (default for server)"
+    echo "        public: CE builds are downloadable (default except server)"
     echo "        only: only upload CE (implies public)"
     echo "        none: do NOT upload CE [optional]"
     echo "  -p: specific platforms to upload. By default uploads all platforms."
@@ -22,21 +22,13 @@ function usage() {
     echo
 }
 
-DEFAULT_PLATFORMS=(ubuntu amzn2 centos debian rhel macos oel suse windows linux)
-MP=
 LIVE=false
-
-# Set to "private" to keep community builds non-downloadable
-COMMUNITY=private
-
-# Default product
-PRODUCT=couchbase-server
 
 while getopts "r:v:V:b:t:s:c:p:lh?" opt; do
     case $opt in
         r) RELEASE=$OPTARG;;
         v) VERSION=$OPTARG;;
-        b) BUILD=$OPTARG;;
+        b) BLD_NUM=$OPTARG;;
         t) PRODUCT=$OPTARG;;
         s) SUFFIX=$OPTARG;;
         c) COMMUNITY=$OPTARG;;
@@ -50,36 +42,60 @@ while getopts "r:v:V:b:t:s:c:p:lh?" opt; do
     esac
 done
 
-if [ ${#PLATFORMS[@]} -eq 0 ]; then
-    PLATFORMS=("${DEFAULT_PLATFORMS[@]}")
-fi
-
-if [ "x${RELEASE}" = "x" ]; then
-    echo "Release code name not set"
+if [ "x${PRODUCT}" = "x" ]; then
+    echo "Product not set"
     usage
     exit 2
+fi
+
+if [ "x${COMMUNITY}" = "x" ]; then
+    if [ "${PRODUCT}" = "couchbase-server" ]; then
+        COMMUNITY=private
+    else
+        # Set to "public" which means "all files" - appropriate for majority of
+        # products that don't have EE/CE split. Also only Server has the
+        # "sometimes we release CE, sometimes we don't" thing.
+        COMMUNITY=public
+    fi
+fi
+
+if [ ${#PLATFORMS[@]} -eq 0 ]; then
+    if [ "${PRODUCT}" = "couchbase-server" ]; then
+        # For Server 7.2.2 and later, only release generic linux, macos, windows
+        if [ "7.2.2" = $(printf "7.2.2\n${VERSION}" | sort -n | head -1) ]; then
+            PLATFORMS=(linux macos windows)
+        else
+            PLATFORMS=(ubuntu amzn2 centos debian rhel macos oel suse windows linux)
+        fi
+    else
+        # This is a "no-op" platform wildcard, since all filenames will have
+        # at least one - in them.
+        PLATFORMS=(-)
+    fi
 fi
 
 if [ "x${VERSION}" = "x" ]; then
-    echo "Version number not set"
+    echo "Version not set"
     usage
     exit 2
 fi
 
-if [ "x${BUILD}" = "x" ]; then
+if [ "x${RELEASE}" = "x" ]; then
+    RELEASE=${VERSION}
+fi
+
+if [ "x${BLD_NUM}" = "x" ]; then
     echo "Build number not set"
     usage
     exit 2
 fi
 
-if ! [[ $VERSION =~ ^[0-9]*\.[0-9]*\.[0-9]*$ ]]
-then
+if ! [[ $VERSION =~ ^[0-9]*\.[0-9]*\.[0-9]*$ ]]; then
     echo "Version number format incorrect. Correct format is A.B.C where A, B and C are integers."
     exit 3
 fi
 
-if ! [[ $BUILD =~ ^[0-9]*$ ]]
-then
+if ! [[ $BLD_NUM =~ ^[0-9]*$ ]]; then
     echo "Build number must be an integer"
     exit 3
 fi
@@ -98,8 +114,7 @@ fi
 
 
 # Compute target filename components
-if [ -z "$SUFFIX" ]
-then
+if [ -z "$SUFFIX" ]; then
     RELEASE_DIRNAME=$VERSION
     FILENAME_VER=$VERSION
 else
@@ -117,8 +132,7 @@ fi
 # just to avoid needing to check whether they exist first.
 
 # Compute root destination directories, creating them as necessary.
-if [[ "$LIVE" = "true" ]]
-then
+if [[ "$LIVE" = "true" ]]; then
     S3_ROOT=s3://packages.couchbase.com/releases
     RELEASE_ROOT=${RELEASES_MOUNT}
 else
@@ -129,8 +143,7 @@ fi
 
 # Add product super-directory, if not couchbase-server. Create if
 # necessary.
-if [[ "${PRODUCT}" != "couchbase-server" ]]
-then
+if [[ "${PRODUCT}" != "couchbase-server" ]]; then
     RELEASE_DIRNAME=${PRODUCT}/${RELEASE_DIRNAME}
     mkdir -p -m 755 ${RELEASE_ROOT}/${PRODUCT}
 fi
@@ -144,8 +157,7 @@ upload()
 {
     echo ::::::::::::::::::::::::::::::::::::::
 
-    if [[ "$COMMUNITY" == "private" ]]
-    then
+    if [[ "$COMMUNITY" == "private" ]]; then
         echo Uploading ${RELEASE_DIRNAME} ...
         echo CE are uploaded PRIVATELY ...
         perm_arg="private"
@@ -167,38 +179,30 @@ finish() {
 }
 trap finish EXIT
 
-if [ ! -e ${LB_MOUNT}/${PRODUCT}/$RELEASE/$BUILD ]; then
-    echo "Given build doesn't exist: ${LB_MOUNT}/${PRODUCT}/$RELEASE/$BUILD"
+if [ ! -e ${LB_MOUNT}/${PRODUCT}/$RELEASE/$BLD_NUM ]; then
+    echo "Given build doesn't exist: ${LB_MOUNT}/${PRODUCT}/$RELEASE/$BLD_NUM"
     exit 5
 fi
 
-#prepare files to be uploaded
+# Prepare files to be uploaded
 UPLOAD_TMP_DIR=/tmp/${PRODUCT}-${RELEASE}-${BLD_NUM}
 rm -rf ${UPLOAD_TMP_DIR} && mkdir -p ${UPLOAD_TMP_DIR}
 
-cd ${LB_MOUNT}/${PRODUCT}/$RELEASE/$BUILD
+cd ${LB_MOUNT}/${PRODUCT}/$RELEASE/$BLD_NUM
 
 # Copy manifest and notices.txt to release directory
-cp ${PRODUCT}-${VERSION}-${BUILD}-manifest.xml ${UPLOAD_TMP_DIR}/${PRODUCT}-${VERSION}-manifest.xml
-NOTICES_FILE=blackduck/${PRODUCT}-${VERSION}-${BUILD}-notices.txt
+cp ${PRODUCT}-${VERSION}-${BLD_NUM}-manifest.xml ${UPLOAD_TMP_DIR}/${PRODUCT}-${VERSION}-manifest.xml
+NOTICES_FILE=blackduck/${PRODUCT}-${VERSION}-${BLD_NUM}-notices.txt
 if [ -e ${NOTICES_FILE} ]; then
     cp ${NOTICES_FILE} ${UPLOAD_TMP_DIR}/${PRODUCT}-${VERSION}-notices.txt
 fi
 
-# For Server 7.2.0 and up, at the moment we produce some installers that we
-# don't want to ship. Filter those out.
-if [ "7.2.0" = $(printf "7.2.0\n${VERSION}" | sort -n | head -1) ]; then
-    SKIP_PLATFORMS=(centos7 oel7 rhel7 ubuntu18.04)
-fi
-for skip in ${SKIP_PLATFORMS[@]}; do
-    EXTRA_FIND_ARGS="${EXTRA_FIND_ARGS} -not -name *${skip}*"
-done
-
 for platform in ${PLATFORMS[@]}
 do
-    # Have to disable bash's filename expansion here (with "set -f") -
-    # doesn't seem to be any other way to pass EXTRA_FIND_ARGS to find
-    # without bash expanding the glob wildcards first
+    # Have to disable bash's filename expansion here (with "set -f") - doesn't
+    # seem to be any other way to pass EXTRA_FIND_ARGS to find without bash
+    # expanding the glob wildcards first. (EXTRA_FIND_ARGS isn't used anymore,
+    # but leaving it in case we need it again in future.)
     for file in $( \
         set -f; \
         find . -maxdepth 1 \( \
@@ -209,6 +213,9 @@ do
             -not -name *.md5 \
             -not -name *.sha26 \
             -not -name *.properties \
+            -not -name *properties.json \
+            -not -name *-manifest.xml \
+            -not -name *-source.tar.gz \
             ${EXTRA_FIND_ARGS} \
         \) )
     do
@@ -216,16 +223,14 @@ do
         file=${file/.\//}
 
         # "artifact" is the filename with the build number stripped out
-        artifact=${file/$VERSION-$BUILD/$FILENAME_VER}
+        artifact=${file/$VERSION-$BLD_NUM/$FILENAME_VER}
 
         # Handle various options for CE artifacts
-        if [[ "$COMMUNITY" == "none" && "${artifact}" =~ "community" ]]
-        then
+        if [[ "$COMMUNITY" == "none" && "${artifact}" =~ "community" ]]; then
             echo "COMMUNITY=none set, skipping ${artifact}"
             continue
         fi
-        if [[ "$COMMUNITY" == "only" && ! "${artifact}" =~ "community" ]]
-        then
+        if [[ "$COMMUNITY" == "only" && ! "${artifact}" =~ "community" ]]; then
             echo "COMMUNITY=only set, skipping ${artifact}"
             continue
         fi
