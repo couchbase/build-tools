79fc319a197b7649e5b0fd25613e67aad92b7462
Chris Hillery <ceej@couchbase.com>
2023-09-27 16:08:17 -0700

    CBD-5408: k8s-release uses standard release_binaries_to_s3
    Allows k8s-release jobs to benefit from the improvements made to
    that job's scripts, including uploading notices files and more
    customizable URL reports
    
    Change-Id: Ic480093209b5e1616a884a8c0024d3de8efd4cd1
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/191069
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- k8s-microservice/jenkins/couchbase-k8s-microservice-release.sh
+++ k8s-microservice/jenkins/couchbase-k8s-microservice-release.sh
@@ -39,76 +39,20 @@ case "${PRODUCT}" in
         ;;
 esac
 
-# For convenience, save a trigger.properties for update_manifest_released.
+# For convenience, save a trigger.properties for update_manifest_released
+# and release_binaries_to_s3.
 # Do this after the above check so that we will only propose changes for
-# images that have corresponding manifests.
+# images that have corresponding manifests and artifacts.
 cat <<EOF > trigger.properties
 PRODUCT_PATH=${PRODUCT}
 RELEASE=${VERSION}
 VERSION=${VERSION}
 BLD_NUM=${BLD_NUM}
 SUFFIX=${suffix}
+APPROVAL_ISSUE=${APPROVAL_ISSUE}
 EOF
 
-################### ARTIFACTS
-
-# Upload artifacts to S3
-upload_path=${PRODUCT}/${public_tag}
-upload_url_base=packages.couchbase.com/${upload_path}
-release_dir=/releases/${upload_path}
-mkdir -p ${release_dir}
-pushd ${release_dir}
-shopt -s nullglob
-UPLOADS=
-for file in /latestbuilds/${PRODUCT}/${VERSION}/${BLD_NUM}/*${BLD_NUM}*; do
-    if [[ $file =~ .*source.tar.gz ]]; then
-        echo Skipping source code file $file
-        continue
-    fi
-    filename=$(basename ${file/${VERSION}-${BLD_NUM}/${public_tag}})
-    cp -av ${file} ${filename}
-    sha256sum ${filename} > ${filename}.sha256
-    if [[ "${PRODUCT}" != "couchbase-operator" && $file =~ ${PRODUCT}-image.* ]]; then
-        echo Skipping upload of internal image file $file
-        continue
-    fi
-    aws s3 cp ${filename} \
-        s3://${upload_url_base}/${filename} --acl public-read
-    aws s3 cp --content-type "text/plain" ${filename}.sha256 \
-        s3://${upload_url_base}/${filename}.sha256 --acl public-read
-    if [[ ! $file =~ .*manifest.* && ! $file =~ .*properties.* ]]; then
-        UPLOADS=1
-    fi
-done
-
-if [ "${UPLOADS}" = "1" ]; then
-    ignorefiles="manifest|properties"
-    links=$(aws s3 ls s3://${upload_url_base}/ | egrep -v "$ignorefiles" | awk -v s3dir=https://${upload_url_base}/ '{print s3dir $4}')
-    rel=$(echo "$links" | grep -v ".sha256")
-    rel_sha=$(echo "$links" | grep ".sha256")
-
-    set +x
-    echo :::::::::::::::::::::::::::::::
-    echo UPLOADED URLS
-    echo :::::::::::::::::::::::::::::::
-
-    echo "Binaries:"
-    echo
-    echo "$rel"
-    echo
-    echo "Shas:"
-    echo
-    echo "$rel_sha"
-    echo
-    echo :::::::::::::::::::::::::::::::
-    set -x
-fi
-
-popd
-
-# Add git tag for release - this should take place after the S3 upload
-# to ensure artifacts are available to any subsequent workflow
-# execution.
+# Add git tag for release.
 # As a heuristic special case, if OS_BUILD is not 1 we assume this is
 # an emergency re-release of RHCC, and therefore git tagging was
 # probably already done, so skip it.
