57490ae55d65e7f08a8fe10b93c4118b323f83e0
Ming Ho <ming.ho@couchbase.com>
2023-10-13 10:02:36 -0700

    CBD-5651 fix tableau connector BD scan.
    -Ming
    
    Change-Id: Ifcd63a2ae29efa6d28ba979b8b8087f2400940d4
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/198346
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/couchbase-tableau-connector/get_additional_source.sh
+++ blackduck/couchbase-tableau-connector/get_additional_source.sh
@@ -1,50 +1,17 @@
 #!/bin/bash -ex
 
-#dependency:go-offline target does not work here since dependent jdbc connector is part of the build.
-#Hence, need to do a full build to actually get all the dependencies.
+MAVEN_VERSION=3.9.5
+cbdep install -d ${WORKSPACE}/extra mvn ${MAVEN_VERSION}
+mv ${WORKSPACE}/extra/mvn-${MAVEN_VERSION} ${WORKSPACE}/extra/mvn
+export PATH=${WORKSPACE}/extra/mvn/bin:$PATH
 
-JDK_VERSION=11.0.9+11
-MAVEN_VERSION=3.5.2-cb6
-MINIFORGE3_VERSION=4.11.0-4
-mkdir ../deps
-pushd ../deps
-
-cbdep install openjdk ${JDK_VERSION} -d .
-cbdep install maven ${MAVEN_VERSION} -d .
-cbdep install miniforge3 ${MINIFORGE3_VERSION} -d .
-export PATH=$(pwd)/miniforge3-${MINIFORGE3_VERSION}/bin:$(pwd)/maven-${MAVEN_VERSION}/bin:$(pwd)/openjdk-${JDK_VERSION}/bin:$PATH
-export JAVA_HOME=$(pwd)/openjdk-${JDK_VERSION}
-
-python3 -m venv couchbase-tableau-connector --clear
-source couchbase-tableau-connector/bin/activate
-
-popd
-
-#Call maven target to replace the *-SNAPSHOT version with ${VERSION} in the pom
-
-mvn -B versions:set \
-    -DnewVersion=${VERSION} \
-    -DgenerateBackupPoms=false \
+mvn dependency:copy-dependencies \
+    -DincludeScope=runtime \
     -f cbtaco/pom.xml
 
-mvn -B install \
-    -DskipTests \
-    -Dpython.path=$(which python3) \
-    -f cbtaco/pom.xml
-
-#Get additional dependencie from Couchase JVM Clients
-#Only core-io-deps is used by tabealu jdbc connector, hence the rest is removed.
-COREIO_VERSION=`xmllint --xpath '//*[local-name()="profiles"]/*[local-name()="profile"]/*[local-name()="id"][contains(., "analytics-taco-couchbase-jdbc-repo")]/following-sibling::*[local-name()="properties"]/*[local-name()="couchbase-coreio.version"]/text()' cbtaco/cbas/pom.xml`
-
-pushd cbtaco/couchbase-jdbc-driver
-git clone https://github.com/couchbase/couchbase-jvm-clients
-pushd couchbase-jvm-clients
-git checkout core-io-${COREIO_VERSION}
-cd core-io-deps
-mvn -B install -DskipTests
+mv ${WORKSPACE}/src/cbtaco/cbas/cbas-jdbc-taco/target/dependency ${WORKSPACE}/src/cbtaco/.
+pushd ${WORKSPACE}/src/cbtaco/dependency
+for file in $(ls *.jar); do
+  jar -xf $file
+done
 popd
-mv couchbase-jvm-clients/core-io-deps .
-rm -rf couchbase-jvm-clients
-popd
-
-deactivate
