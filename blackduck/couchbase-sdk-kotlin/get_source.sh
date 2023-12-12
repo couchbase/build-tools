1d90b5943fa0ccbdbe905f62dac6002c9e394e01
David Nault <david.nault@couchbase.com>
2023-12-12 13:43:59 -0800

    Black Duck: Add couchbase-sdk-kotlin
    Change-Id: If49a4eaf516b5d56b9a0a4f424c2edea78eaa1c2
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/202526
    Reviewed-by: David Nault <david.nault@couchbase.com>
    Tested-by: David Nault <david.nault@couchbase.com>

---
+++ blackduck/couchbase-sdk-kotlin/get_source.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ex
+
+PRODUCT=$1
+RELEASE=$2
+VERSION=$3
+BLD_NUM=$4
+
+git clone ssh://git@github.com/couchbase/couchbase-jvm-clients
+pushd couchbase-jvm-clients
+TAG="kotlin-client-${VERSION}"
+if git rev-parse --verify --quiet $TAG >& /dev/null
+then
+    echo "$TAG exists, checking it out"
+    git checkout $TAG
+else
+    echo "No $TAG tag or branch, assuming master"
+fi
+
+# The fit-performer packages are test-only and require a non-public
+# jar, so they'll never be shipped; but their poms mess up the scans.
+rm -rf *-fit-performer
+
+# And now we actually need to build stuff for it to be found
+# by the detector
+mvn --batch-mode dependency:resolve || {
+    for project in protostellar core-io-deps test-utils tracing-opentelemetry-deps . ; do
+        if [ -e "$project" ]; then
+            mvn --batch-mode -f "$project/pom.xml" -Dmaven.test.skip=true clean install
+        fi
+    done
+}
+
+popd
