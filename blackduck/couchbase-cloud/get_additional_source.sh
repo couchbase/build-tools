bf7db7bb8e3b9a80974da7eae267550fbd60c4ad
Chris Hillery <ceej@couchbase.com>
2024-01-29 14:40:45 -0800

    Use npm from blackduck-scan VM
    couchbase-cloud now requires a newer npm than was specified in this
    script. Newer versions from cbdep don't run on the BD scan VM (requires
    a newer glibc), so I built npm 20 from source on the VM after installing
    gcc 8. Use that for now, until we can update the VM to a newer OS or
    move it to a Docker container.
    
    Change-Id: If12c33a5b2fb25d76b9d4011a9d458a9b5eb81f7
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/204542
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-cloud/get_additional_source.sh
+++ blackduck/couchbase-cloud/get_additional_source.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -ex
 
-## blackduck complains about node_modules directory if it is not there.
+# blackduck complains about node_modules directory if it is not there,
+# so run "npm" install for all packages.
 
-NODE_VERSION=16.15.1
-cbdep install -d ${WORKSPACE}/extra nodejs ${NODE_VERSION}
-export PATH=${WORKSPACE}/extra/nodejs-${NODE_VERSION}/bin:$PATH
+# NOTE: this script should install an appropriate version of 'nodejs'
+# using cbdep. However, at the moment due to CBD-4283 this doesn't work.
+# Hence node 20.11.0 has been installed manually (using nvm) on the BD
+# scan VM, and this script assumes it is on $PATH. If we need a newer
+# version than that in future, we can log into the VM and run "nvm
+# install -s <version>" followed by "nvm alias default <version>" to
+# make it the default version on $PATH. This takes a long time (the -s
+# option makes it build nodejs from source, which is required due to
+# CBD-4283).
 
 rm -rf couchbase-cloud/cmd/cp-ui couchbase-cloud/cmd/cp-ui-tests couchbase-cloud/cmd/cp-ui-docs-screenshots
 for dir in $(find . -name package.json \
