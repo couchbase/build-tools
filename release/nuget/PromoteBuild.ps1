ce90d70d9520c39fdce80ceb181e6fdaf1beb14d
Ming Ho <ming.ho@couchbase.com>
2024-03-13 20:51:08 -0700

    CBD-5826, nuget promote and publishing scripts.
    These scripts are migrated from https://github.com/couchbase/build/tree/master/scripts/jenkins/lite-net
    Product param is added to the scripts so that they can be used to promote additional products such as couchbase-lite-net-extensions.
    
    -Ming
    
    Change-Id: I428c53bb4db5186ed6af1decbdf11e9e339b6505
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/207090
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Jim Borden <jim.borden@couchbase.com>

---
+++ release/nuget/PromoteBuild.ps1
@@ -0,0 +1,95 @@
+<#
+.SYNOPSIS
+    A tool for changing the version, and release notes, of a given version of the Couchbase Lite nuget packages
+.DESCRIPTION
+    This tool will unzip the package, change the nuget version and release notes, then repackage in the same directory
+.PARAMETER Product
+    The product name,i.e. couchbase-lite-net, couchbase-lite-net-extensions
+.PARAMETER InVersion
+    The existing version of the libraries to modify
+.PARAMETER OutVersion
+    The version to modify the libraries to
+.EXAMPLE
+    C:\PS> .\PromoteBuild.ps1 -Product couchbase-lite-net -InVersion 3.2.0-b0001 -OutVersion 3.2.0-1
+    Changes couchbase-lite-net libraries's version from 3.2.0-b0001 to 3.2.0-1
+#>
+param(
+    [Parameter(Mandatory=$true, HelpMessage="The product")][string]$Product,
+    [Parameter(Mandatory=$true, HelpMessage="The existing version of the libraries to modify")][string]$InVersion,
+    [Parameter(Mandatory=$true, HelpMessage="The version to modify the libraries to")][string]$OutVersion
+)
+
+function Take-While() {
+    param( [scriptblock]$pred = $(throw "Need a predicate") )
+    begin {
+        $take = $true
+    } process {
+        if($take) {
+            $take = & $pred $_
+            if($take) {
+                $_
+            }
+        } else {
+            return
+        }
+    }
+}
+
+Push-Location $PSScriptRoot
+try {
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $ErrorActionPreference = "Stop"
+
+    $Version31Up = -not $InVersion.StartsWith("1") -and -not $InVersion.StartsWith("2") -and -not $InVersion.StartsWith("3.0")
+    $buildlessVersion = $InVersion.Split("-")[0]
+    $numericBuildNumber = $InVersion.Split("-")[1].TrimStart('b', '0')
+
+    switch ($Product) {
+        "couchbase-lite-net" {
+            if ($Version31Up) {
+                $package_names = "Couchbase.Lite","Couchbase.Lite.Enterprise","Couchbase.Lite.Support.Android","Couchbase.Lite.Support.iOS","Couchbase.Lite.Support.NetDesktop","Couchbase.Lite.Support.UWP","Couchbase.Lite.Support.WinUI","Couchbase.Lite.Enterprise.Support.Android","Couchbase.Lite.Enterprise.Support.iOS","Couchbase.Lite.Enterprise.Support.NetDesktop","Couchbase.Lite.Enterprise.Support.UWP","Couchbase.Lite.Enterprise.Support.WinUI"
+                $snupkg_names = "Couchbase.Lite","Couchbase.Lite.Enterprise"
+            } else {
+                $package_names = "Couchbase.Lite","Couchbase.Lite.Enterprise","Couchbase.Lite.Support.Android","Couchbase.Lite.Support.iOS","Couchbase.Lite.Support.NetDesktop","Couchbase.Lite.Support.UWP","Couchbase.Lite.Enterprise.Support.Android","Couchbase.Lite.Enterprise.Support.iOS","Couchbase.Lite.Enterprise.Support.NetDesktop","Couchbase.Lite.Enterprise.Support.UWP"
+                $snupkg_names = ""
+            }
+        }
+        "couchbase-lite-net-extensions" {
+            $package_names = "Couchbase.Lite.Extensions"
+            $snupkg_names = "Couchbase.Lite.Extensions"
+        }
+        default {
+            Write-Host "$Product is not supported."
+        }
+    }
+    foreach($package in $package_names) {
+        Write-Host "Downloading http://latestbuilds.service.couchbase.com/builds/latestbuilds/$Product/$buildlessVersion/$numericBuildNumber/$package.$InVersion.nupkg"
+        Invoke-WebRequest http://latestbuilds.service.couchbase.com/builds/latestbuilds/$Product/$buildlessVersion/$numericBuildNumber/$package.$InVersion.nupkg -OutFile "$package.$InVersion.nupkg"
+    }
+    foreach($snupkg in $snupkg_names) {
+        Write-Host "Downloading http://latestbuilds.service.couchbase.com/builds/latestbuilds/$Product/$buildlessVersion/$numericBuildNumber/$snupkg.$InVersion.snupkg"
+        Invoke-WebRequest http://latestbuilds.service.couchbase.com/builds/latestbuilds/$Product/$buildlessVersion/$numericBuildNumber/$snupkg.$InVersion.snupkg -OutFile "$snupkg.$InVersion.snupkg"
+    }
+
+    foreach($file in (Get-ChildItem $pwd -Filter *.*nupkg)) {
+        $packageExtension = [System.IO.Path]::GetExtension($file.Name).TrimStart('.')
+        $packageComponents = [System.IO.Path]::GetFileNameWithoutExtension($file.Name).Split('.') | Take-While { -Not [System.Char]::IsDigit($args[0][0]) }
+        $package = [System.String]::Join(".", $packageComponents)
+        Remove-Item -Recurse -Force $package -ErrorAction Ignore
+        New-Item -ItemType Directory $package
+        [System.IO.Compression.ZipFile]::ExtractToDirectory((Join-Path (Get-Location) "$package.$InVersion.$packageExtension"), (Join-Path (Get-Location) $package))
+        Push-Location $package
+        $stringContent = (Get-Content -Path "${package}.nuspec").Replace($InVersion, $OutVersion)
+        $nuspec = [xml]$stringContent
+
+        $nuspec.Save([System.IO.Path]::Combine($pwd, "$package.nuspec"))
+        Pop-Location
+
+        Remove-Item -Path "$package.$OutVersion.$packageExtension" -ErrorAction Ignore -Force
+        & 7z a -tzip "$package.$OutVersion.$packageExtension" ".\$package\*"
+        Remove-Item -Recurse -Force -Path $package
+        Remove-Item -Force -Path "$package.$InVersion.$packageExtension"
+    }
+} finally {
+    Pop-Location
+}
---
+++ release/nuget/PushBuild.ps1
@@ -0,0 +1,47 @@
+﻿<#
+.SYNOPSIS
+    A tool for pushing Couchbase Lite nuget packages to Nuget
+.DESCRIPTION
+    This tool will download the nuget packages from S3, and then push them all to Nuget
+.PARAMETER Product
+    The product to release, i.e. couchbase-lite-net, couchbase-lite-net-extensions.
+.PARAMETER Version
+    The version of the library to download from S3
+.PARAMETER AccessKey
+    The AWS access key
+.PARAMETER SecretKey
+    The AWS secret key
+.PARAMETER NugetApiKey
+    The API key for pushing to the Nuget feed
+.PARAMETER DryRun
+    Perform all steps except for the actual Nuget feed push
+.EXAMPLE
+    C:\PS> .\PushBuild.ps1 -Product couchbase-lite-net -Version 3.2.0 -AccessKey <key> -SecretKey <key> -NugetApiKey <key>
+    Pushes the official couchbase-lite-net 3.2.0 packages to nuget.org
+#>
+[CmdletBinding(DefaultParameterSetName='set2')]
+param(
+    [Parameter(ParameterSetName='set2', Mandatory=$true, HelpMessage="The product to release")][string]$Product,
+    [Parameter(ParameterSetName='set2', Mandatory=$true, HelpMessage="The version to download from S3")][string]$Version,
+    [Parameter(ParameterSetName='set2', Mandatory=$true, HelpMessage="The access key of the AWS credentials")][string]$AccessKey,
+    [Parameter(ParameterSetName='set2', Mandatory=$true, HelpMessage="The secret key of the AWS credentials")][string]$SecretKey,
+    [Parameter(ParameterSetName='set2', Mandatory=$true, HelpMessage="The API key for pushing to the Nuget feed")]
+    [Parameter(ParameterSetName='set1', Mandatory=$true, HelpMessage="The API key for pushing to the Nuget feed")][string]$NugetApiKey,
+    [Parameter(ParameterSetName='set2')][switch]$DryRun
+)
+
+Write-Host "Downloading packages from S3..."
+Read-S3Object -BucketName packages.couchbase.com -KeyPrefix releases/${Product}/${Version} -Folder . -AccessKey $AccessKey -SecretKey $SecretKey
+$NugetUrl = "https://api.nuget.org/v3/index.json"
+
+if(-Not $(Test-Path .\nuget.exe) -and -not $DryRun) {
+    Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v5.5.1/nuget.exe -OutFile nuget.exe
+}
+
+foreach($file in (Get-ChildItem $pwd -Filter *.nupkg)) {
+    if($DryRun) {
+        Write-Host "DryRun specified, skipping push for $file"
+    } else {
+        & .\nuget.exe push $file -ApiKey $NugetApiKey -Source $NugetUrl
+    }
+}
