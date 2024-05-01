3fd7a555e7e3921c1ea1872246cb88551d4d50e3
Chris Hillery <ceej@couchbase.com>
2024-05-01 15:12:27 -0700

    MB-60697: Enable openblas builds on Windows for Server
    After some experimentation, it seems that we are happy using the
    same openblas build logic as couchbase-lite-vector-search.
    
    Change-Id: I9167586de3b23de022013a94c664866e069c9767
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/209352
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- cbdeps/openblas/openblas_windows.bat
+++ cbdeps/openblas/openblas_windows.bat
@@ -1,8 +1,2 @@
-set PROFILE=%4
-
-if "%PROFILE%" == "lite" (
-    powershell -File %~dp0\openblas_lite_windows.ps1 -InstallDir %1 -RootDir %2 -Arch %8
-) else (
-    echo "ERROR: Unrecognized profile %PROFILE%"
-    exit 1
-)
+rem For now at least, Server and Lite want the same things
+powershell -File %~dp0\openblas_lite_windows.ps1 -InstallDir %1 -RootDir %2 -Arch %8
