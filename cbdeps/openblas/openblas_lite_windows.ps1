6c98aaa6e5795ed3b739305652fec22559cd40c2
Jim Borden <jim.borden@couchbase.com>
2024-05-09 08:29:47 +0900

    Add some additional check in OpenBLAS windows lite
    Fail if cmake or ninja fail, and check for required Visual Studio components.  Also, no need to check for cbdep or cmake since they are provided by the build system
    
    Change-Id: I2aa61340cc03f8b350419728939dc1ba159e801e
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/209696
    Tested-by: Jim Borden <jim.borden@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- cbdeps/openblas/openblas_lite_windows.ps1
+++ cbdeps/openblas/openblas_lite_windows.ps1
@@ -8,7 +8,7 @@
 .PARAMETER RootDir
     The top level workspace that this script is meant to operate in
 .PARAMETER Architectures
-    The architectures to build (x86_64, arm64, or both)
+    The architectures to build (x86_64 / amd64 or arm64)
 #>
 
 param(
@@ -19,39 +19,51 @@ param(
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls13
 
-$VS_VER=$(& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.VisualStudio.Component.VC.Llvm.Clang -property catalog_productLineVersion)
-if($VS_VER -ne "2022") {
-    if(-Not $VS_VER) {
-        throw "No Visual Studio detected with clang-cl, giving up..."
-    }
-
-    throw "clang-cl found, but Visual Studio version invalid ($VS_VER)"
+if($Arch -eq "x86_64" -or $Arch -eq "amd64") {
+    $ToolsComponent = "Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
+} elseif($Arch -eq "arm64") {
+    $ToolsComponent = "Microsoft.VisualStudio.Component.VC.Tools.ARM64"
+} else {
+    throw "Unrecognized architecture $Arch"
 }
+
+function Test-Component() {
+    param(
+        [Parameter(Mandatory=$true)][string]$Name
+    )
+
+    $VS_VER=$(& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires $Name -property catalog_productLineVersion)
+    if($VS_VER -ne "2022") {
+        if(-Not $VS_VER) {
+            throw "No Visual Studio detected with workload '$Name', giving up..."
+        }
+
+        throw "Workload '$Name' found, but Visual Studio version invalid ($VS_VER)"
+    }
+}
+
+Test-Component -Name "Microsoft.VisualStudio.Component.VC.Llvm.Clang"
+Test-Component -Name $ToolsComponent
 
 if(-Not (& where.exe cbdep)) {
     throw "Unable to find cbdep.exe!"
 }
 
-$CMAKE_VER="3.28.1"
+if(-Not (& where.exe cmake)) {
+    throw "Unable to find cmake!"
+}
+
 $NINJA_VER="1.10.1"
 
 New-Item -ItemType Directory -Path $RootDir/openblas/build_$Arch -ErrorAction Ignore
 New-Item -ItemType Directory -Path $RootDir/tools/ -ErrorAction Ignore
-
-$CMAKE="$RootDir/tools/cmake-${CMAKE_VER}/bin/cmake.exe"
-if(-Not (Test-Path $CMAKE)) {
-    Write-Host
-    Write-Host " ======== Installing cmake ========"
-    Write-Host
-    & $RootDir/tools/cbdep.exe install -d $RootDir/tools cmake ${CMAKE_VER}
-}
 
 $NINJA="$RootDir/tools/ninja-${NINJA_VER}/bin/ninja.exe"
 if(-Not (Test-Path $NINJA)) {
     Write-Host
     Write-Host " ======== Installing ninja ========"
     Write-Host
-    & $RootDir/tools/cbdep.exe install -d $RootDir/tools ninja ${NINJA_VER}
+    & cbdep.exe install -d $RootDir/tools ninja ${NINJA_VER}
 }
 
 Write-Host
@@ -60,7 +72,7 @@ Write-Host
 
 Push-Location $RootDir/openblas/build_$Arch
 if($Arch -eq "arm64") {
-    & $CMAKE `
+    cmake `
     -G Ninja `
     -DNOFORTRAN=ON `
     -DCMAKE_CROSSCOMPILING=ON `
@@ -80,7 +92,7 @@ if($Arch -eq "arm64") {
     -DCMAKE_INSTALL_PREFIX="$InstallDir" `
     -S ..
 } else {
-    & $CMAKE `
+    cmake `
     -G Ninja `
     -DNOFORTRAN=ON `
     -DCMAKE_C_COMPILER=clang-cl `
@@ -96,6 +108,10 @@ if($Arch -eq "arm64") {
     -DUSE_THREAD=0 `
     -DUSE_LOCKING=1 `
     -S ..
+}
+
+if($LASTEXITCODE -ne 0) {
+    throw "CMake failed"
 }
 
 function FilterCompileOutput {
@@ -117,3 +133,6 @@ if(Test-Path $RootDir/build.err) {
 
 New-Item -ItemType File $RootDir/build.err
 & $NINJA install | ForEach-Object { FilterCompileOutput -line $_}
+if($LASTEXITCODE -ne 0) {
+    throw "Build failed"
+}
