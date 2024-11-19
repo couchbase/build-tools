c5bf90446e584dcf1d1062d6551cef5992c643dd
Chris Hillery <ceej@couchbase.com>
2024-11-18 22:23:39 -0800

    CBD-5665: Update add_project.py to BD Client interface
    Change-Id: I4e83c670bbd819a3b17fc3af647f2604c6567c6f
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/219015
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- blackduck/jenkins/detect-scan/add_subproject.py
+++ blackduck/jenkins/detect-scan/add_subproject.py
@@ -3,10 +3,11 @@
 import argparse
 import json
 import logging
-import sys, os
+import pprint
+import sys
 from pathlib import Path
 
-from blackduck.HubRestApi import HubInstance, object_id
+from blackduck import Client
 
 # logging
 logger = logging.getLogger()
@@ -33,36 +34,65 @@ if Path(args.sub_project_json).exists():
     sub_project_list = json.loads(open(args.sub_project_json).read())['sub-projects']
     logger.info('Loading ' + args.sub_project_json)
 else:
-    sys.exit('Unable to locate ' + sub_project_json)
-hub = HubInstance(bd_creds['url'], bd_creds['username'], bd_creds['password'], insecure=True)
+    sys.exit('Unable to locate ' + args.sub_project_json)
+client = Client(base_url=bd_creds["url"], token=bd_creds["token"])
 
-parent_project = hub.get_project_version_by_name(args.parent_project, args.version)
-if parent_project is None:
-    #unable to add sub-project when project or its version doesn't exist
-    sys.exit(parent_project+' or its version is NOT found.')
+parent_project_version = None
+try:
+    parent_project = next(client.get_resource('projects', params={"q": f"name:{args.parent_project}"}))
+except StopIteration:
+    sys.exit(f"Project {args.parent_project} not found!")
+try:
+    parent_project_version = next(client.get_resource('versions', parent_project, params={"q": f"versionName:{args.version}"}))
+except StopIteration:
+    sys.exit(f"Version {args.version} for project {args.parent_project} not found!")
 
 #create parent/sub project relationships
 for sub_project_name in sub_project_list:
-    sub_project = hub.get_project_by_name(sub_project_name)
-    if sub_project is None:
-        sys.exit(sub-project +' does not exist on blackduck. It can NOT be added as a sub-project.')
-    sub_project_versions = hub.get_project_versions(sub_project)
-    if 'totalCount' in sub_project_versions and sub_project_versions['totalCount'] > 0:
-        #Get all version of the sub-project from blackduck
-        #Find the latest of x.y.z and add it as the sub-project
-        #For example:
-        #  couchbase-lite-core has 2.7.4, 2.8.0, 2.8.4, 3.0.0
-        #    2.7.x is mercury
-        #    2.8.x is hydrogen
-        #    3.0.x is lithium
-        #  when adding it as sub-project for CBL android 2.8.5, the version to match is 2.8.
-        #  and 2.8.4 should be picked.
-        versions = sub_project_versions['items']
-        sorted_versions = sorted(versions, key=lambda k: k['versionName'], reverse=True)
-        version_to_match=args.version.rsplit('.', 1)[0]
-        index = next((index for (index, d) in enumerate(sorted_versions) if version_to_match in d['versionName']), None)
-        if index is None:
-            sys.exit("No matching version is found for sub-project: " + sub_project_name)
-        hub.add_version_as_component(parent_project, sorted_versions[index])
-    else:
-        sys.exit('No version is found on blackduck for ' + sub_project_name)
+    try:
+        sub_project = next(client.get_resource('projects', params={"q": f"name:{sub_project_name}"}))
+    except StopIteration:
+        sys.exit(f"Sub-project {sub_project_name} not found!")
+
+    #Get all version of the sub-project from blackduck
+    #Find the latest of x.y.z and add it as the sub-project
+    #For example:
+    #  couchbase-lite-core has 2.7.4, 2.8.0, 2.8.4, 3.0.0
+    #    2.7.x is mercury
+    #    2.8.x is hydrogen
+    #    3.0.x is lithium
+    #  when adding it as sub-project for CBL android 2.8.5, the version to match is 2.8.
+    #  and 2.8.4 should be picked.
+    version_to_match=args.version.rsplit('.', 1)[0]
+    sub_project_versions = [
+        v for v in client.get_resource('versions', sub_project)
+        if v['versionName'].startswith(version_to_match)
+    ]
+    sub_project_versions = sorted(
+        sub_project_versions, key=lambda k: k['versionName'],
+        reverse=True
+    )
+    if len(sub_project_versions) < 1:
+        sys.exit(f"No version {version_to_match}.x is found on blackduck for {sub_project_name}")
+
+    # Dig out the parent project-version's "components" URL
+    parent_components = client.list_resources(parent_project_version)['components']
+
+    # Convert the subproject-version to a "components" URL (yes, apparently
+    # you can kinda just do this)
+    sub_project_component = \
+        client.list_resources(sub_project_versions[0])['href'].replace(
+            '/projects/', '/components/'
+        )
+
+    # POST this sub-project component to the parent's components
+    payload = {
+        "component": sub_project_component
+    }
+    headers = {
+        'accept': "application/json",
+        'content-type': "application/vnd.blackducksoftware.bill-of-materials-6+json"
+    }
+    response = client.session.post(parent_components, json=payload, headers=headers)
+    response.raise_for_status()
+    print(f"Added {sub_project_name} to {args.parent_project}")
