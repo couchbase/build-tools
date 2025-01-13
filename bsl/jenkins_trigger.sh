d07da2fc201a4e5311583099cf295329cc77b0d9
Chris Hillery <ceej@couchbase.com>
2025-01-13 13:22:13 -0800

    CBD-5750: Support GitHub PRs for BSL updates
    A project in a manifest may specify the following annotations:
    
       <annotation name="BSL_REVIEW" value="github-pr" keep="true"/>
       <annotation name="BSL_REVIEWERS" value="user1,user2" keep="true"/>
    
    The project must also specify the attribute "dest-branch" if it is not
    referencing the correct target branch (eg., a submodule locked to a
    SHA).
    
    Also:
    
    - Ensure the `gh` tool is available, for creating PRs.
    - Skip manifests with "do-build": false.
    - Make job output a bit easier to parse.
    
    Change-Id: I12ac0c855f7f3b9bc73d9da478465421695b35fc
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/221646
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- bsl/jenkins_trigger.sh
+++ bsl/jenkins_trigger.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SCRIPT_DIR}/../utilities/shell-utils.sh
 
 NODE_VERSION=17.1.0
+GH_VERSION=2.65.0
 
 # Set script args based on env vars from Jenkins parameters
 ${DRY_RUN} && DRY_RUN_ARG=-n
@@ -44,12 +45,36 @@ else
     MANIFESTS=${MANIFEST}
 fi
 
+# Set up environment for scripts: set GH_TOKEN, install cbdep, install
+# gh. The `gh` tool will honor the GH_TOKEN env var for authentication.
+# It's safer to use that here rather than `gh auth login` because the
+# latter updates the Unix user's global settings, which is an
+# unnecessary risk for a shared environment like a Jenkins agent.
+stop_xtrace
+export GH_TOKEN=$(cat ~/.ssh/cb-robot-bsl-jenkins-token)
+restore_xtrace
+
+uv tool install --upgrade cbdep
+cbdep install -d tools gh ${GH_VERSION}
+export PATH=$(pwd)/tools/gh-${GH_VERSION}/bin:$PATH
+
 for mani in ${MANIFESTS}; do
-    header "Checking and updating ${mani}"
-    ./update-bsl-for-manifest \
-        ${DRY_RUN_ARG} ${LICENSES_ONLY_ARG} ${PROJECTS_ARG} \
-        -u "${MANIFEST_REPO}" \
-        -m "${mani}"
+
+    product=$(dirname ${mani})
+    # Skip manifest if `do-build` is explicitly false. `do-build` is
+    # used by `scan-manifests` to determine which manifests to
+    # automatically poll, and that script assumes the default value is
+    # `True`, so we do the same here.
+    do_build=$(cat manifests/${product}/product-config.json | jq '.manifests."'${mani}'"."do-build"')
+    if [ "${do_build}" = "false" ]; then
+        header "${mani}: Skipping because 'do-build' is false"
+    else
+        ./update-bsl-for-manifest \
+            ${DRY_RUN_ARG} ${LICENSES_ONLY_ARG} ${PROJECTS_ARG} \
+            -u "${MANIFEST_REPO}" \
+            -m "${mani}"
+    fi
+
 done
 
 status "All done!"
