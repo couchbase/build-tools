9e1aa09587ac3ef0bc94d56a03b9bd059998704c
Ming Ho <ming.ho@couchbase.com>
2025-02-12 21:32:06 -0800

    CBD-5571, Improvements to cocoapods publishing
    * Retire python script.  It is much easier to do with sed
    * Move bash script from Jenkins job under source control
    * Raise PR after publishing for couchbase-lite-ios
    
    -Ming
    
    Change-Id: I5a08de4696bc14d17f04a55f062485117a370c30
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/223100
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Vlad Velicu <vlad.velicu@couchbase.com>

---
+++ release/cocoapods/cocoapods_publish.sh
@@ -0,0 +1,137 @@
+#!/bin/bash -e
+
+# Function to update and push podspec
+cocoa_push() {
+    local podspec="${1}"
+    echo "Updating podspec: ${podspec}"
+
+    sed -i \
+        -e "/s\.version/ s/'[^']*'/'${VERSION}'/" \
+        -e "/s\.source/ s/\([0-9]\+\.[0-9]\+\.[0-9]\+\)/${VERSION}/g" \
+        -e "/s\.ios\.deployment_target/ s/'[^']*'/'${ios_target}'/" \
+        -e "/s\.osx\.deployment_target/ s/'[^']*'/'${osx_target}'/" \
+        ${podspec}
+
+    pod spec lint ${podspec}
+
+    # Push to trunk if not in dry-run mode
+    if [[ "${DRYRUN}" == 'false' ]]; then
+        echo "Publish ${podspec}"
+        pod trunk push "${podspec}"
+    else
+        echo "DRYRUN mode: ${podspec}"
+        cat "${podspec}"
+    fi
+}
+
+# Create PR to update couchbase-lite-ios CE podspecs
+create_pr() {
+    local repo="${1}"
+    pushd ${SCRIPT_DIR}/${repo}
+    git checkout ${BRANCH}
+    git branch podspecs_update
+    git checkout podspecs_update
+    for podspec in *.podspec; do
+        sed -i \
+            -e "/^\s*s\.version/ s/'[^']*'/'${VERSION}'/" \
+            -e "/s\.ios\.deployment_target/ s/'[^']*'/'${ios_target}'/" \
+            -e "/s\.osx\.deployment_target/ s/'[^']*'/'${osx_target}'/" \
+            ${podspec}
+    done
+    if [[ -z $(git status -s) ]]; then
+        echo "Podspecs are up-to-date.  No need to raise PR."
+    else
+        git commit -a -m "Update pod specs for ${PRODUCT} ${VERSION}"
+
+        if [[ "${DRYRUN}" == 'false' ]]; then
+            gh pr create --repo "${repo}" --base "${BRANCH}" -f
+        fi
+    fi
+    popd
+}
+
+# set environment variables for Ruby
+set_ruby_env() {
+    ### zz-lightweight for mobile comes w/ rvm installed
+    ### rvm install will skip installation if desired version is already installed
+    sudo chown -R couchbase:couchbase /usr/share/rvm
+    source /etc/profile.d/rvm.sh
+    rvmsudo rvm install "${RUBY_VERSION}"
+    rvm --default use "${RUBY_VERSION}"
+    rvmsudo gem install cocoapods
+    rvmsudo gem update cocoapods
+    export LANG=en_US.UTF-8
+    export PATH="/usr/share/rvm/rubies/ruby-${RUBY_VERSION}/bin:/usr/share/rvm/gems/ruby-${RUBY_VERSION}/bin:$PATH"
+}
+
+# Ensure there is an active pod session.
+# Refers to the confluence page if a new session needs to be initiated
+# https://confluence.issues.couchbase.com/wiki/spaces/CR/pages/2405402255/Mobile+-+Build+and+Release+Process
+verify_pod_session() {
+    echo "Importing GPG keys..."
+    sudo gpg --keyserver keyserver.ubuntu.com \
+        --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    echo "Listing current pod session, if any ..."
+    pod trunk me || exit 1
+}
+
+couchbase_lite_ios_publish() {
+    # Clone repositories and process podspecs
+    git clone git@github.com:couchbaselabs/couchbase-lite-ios-ee.git
+    git clone git@github.com:couchbase/couchbase-lite-ios.git
+
+    # Figure out deployment targets
+    ios_target=$(cat ${SCRIPT_DIR}/couchbase-lite-ios/xcconfigs/Project.xcconfig |grep IPHONEOS_DEPLOYMENT_TARGET |awk '{print $3}')
+    osx_target=$(cat ${SCRIPT_DIR}/couchbase-lite-ios/xcconfigs/Project.xcconfig |grep MACOSX_DEPLOYMENT_TARGET |awk '{print $3}')
+
+    pushd couchbase-lite-ios-ee/Podspecs
+    git checkout ${BRANCH}
+    if [[ "${COMMUNITY}" != "no" ]]; then
+        files=$(ls *.podspec)
+        create_pr "couchbase-lite-ios"
+    else
+        files=$(ls *.podspec |grep Enterprise)
+    fi
+    echo "podspecs: ${files}"
+    for file in ${files}; do
+        cocoa_push "${file}"
+    done
+    popd
+}
+
+couchbase_lite_vector_search_publish() {
+    repo="couchbaselabs/mobile-vector-search.git"
+    git clone git@github.com:${repo}
+    pushd mobile-vector-search/podspec
+    git checkout "${BRANCH}"
+    for file in *.podspec; do
+        cocoa_push "${file}"
+    done
+    popd
+}
+
+# Main
+
+# Predefined params from Jenkins job:
+# ${PRODUCT}, ${VERSION}, ${DRYRUN}, ${BRANCH}, ${COMMUNITY}, ${RUBY_VERSION}
+
+# Ensure there is a valid pod session before continue
+verify_pod_session
+
+set_ruby_env
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd ${SCRIPT_DIR}
+
+case "${PRODUCT}" in
+    couchbase-lite-ios)
+        couchbase_lite_ios_publish
+        ;;
+    couchbase-lite-vector-search)
+        couchbase_lite_vector_search_publish
+        ;;
+    *)
+        echo "Invalid product: ${PRODUCT}"
+        exit 1
+        ;;
+esac
