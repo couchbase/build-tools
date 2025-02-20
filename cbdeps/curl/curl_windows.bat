9c4c28df963548b344cc7c256be893c80f4a32f1
Chris Hillery <ceej@couchbase.com>
2025-02-20 01:45:15 -0800

    CBD-6210: Build updates for curl 8.12.1
    - Convert Windows build to use CMake (older winbuild method is
      deprecated)
    - Update Unix CURL_CA_BUNDLE=env patch
    - Add black-duck-manifest.yaml.in
    
    Change-Id: I96e2ac8353366f878d383a8b73fcb857c744d67d
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/223727
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- cbdeps/curl/curl_windows.bat
+++ cbdeps/curl/curl_windows.bat
@@ -11,18 +11,26 @@ set ZLIB_PATH=%DEPSDIR%\zlib-%ZLIB_VER%
 
 cd %ROOT_DIR%\curl
 
-set OUTPUT_DIR=builds\libcurl-vc17-x64-release-dll-zlib-dll-ipv6-sspi-schannel
-
-call .\buildconf.bat || goto error
 @echo on
-cd winbuild
-call nmake -f Makefile.vc ^
-  mode=dll VC=17 MACHINE=x64 DEBUG=no GEN_PDB=yes ^
-  WITH_ZLIB=dll ZLIB_PATH=%ZLIB_PATH% ^
-  || goto error
+mkdir build
+cd build
+cmake -G Ninja ^
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -DBUILD_SHARED_LIBS=ON ^
+  -D CURL_ZLIB=ON -D ZLIB_ROOT=%ZLIB_PATH% ^
+  -D CURL_USE_LIBPSL=OFF ^
+  -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% .. || goto error
+ninja install || goto error
 
-cd ..
+cd %ROOT_DIR%
 xcopy %OUTPUT_DIR% %INSTALL_DIR% /s || goto error
+
+cd %INSTALL_DIR%
+rmdir /s /q share
+cd bin
+del curl-config mk*.*
+cd ..\lib
+rmdir /s /q pkgconfig
 
 goto :eof
 
