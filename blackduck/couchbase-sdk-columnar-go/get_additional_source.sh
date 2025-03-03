6caeeb98eb61234c84966058dea22ac77f6f14e8
Charles Dixon <chvckd@gmail.com>
2025-03-03 08:20:16 +0000

    Black Duck: Add Go Columnar SDK
    Change-Id: I3642a64ee3ac6fe1aa4cfceadc468e5ba0f44329
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/224232
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Charles Dixon <chvckd@gmail.com>

---
+++ blackduck/couchbase-sdk-columnar-go/get_additional_source.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -ex
+
+cd gocbcolumnar
+go mod download
---
+++ blackduck/couchbase-sdk-columnar-go/get_source.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+PRODUCT=$1
+RELEASE=$2
+VERSION=$3
+BLD_NUM=$4
+
+git clone ssh://git@github.com/couchbase/gocbcolumnar.git
+pushd gocbcolumnar
+TAG="v$VERSION"
+if git rev-parse --verify --quiet $TAG >& /dev/null
+then
+    echo "Tag $TAG exists, checking it out"
+    git checkout $TAG
+else
+    echo "No tag $TAG, assuming master"
+fi
+
+# Add golang version from go.mod to ${WORKSPACE}/extra
+export PATH="$(${WORKSPACE}/build-tools/blackduck/jenkins/util/go-path-from-mod.sh):$PATH"
+
+popd
