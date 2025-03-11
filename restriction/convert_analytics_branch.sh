e76981a8dc48bcd9d1c432a7cebd5720795af7a8
Michael Blow <michael.blow@couchbase.com>
2025-03-11 15:59:03 -0400

    MB-65666: Update stabilization-c8b0f90c72 to resolve to 7.6.5
    Change-Id: Idca22082410e74876a2739fd74ebb2153f5b1327
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/224725
    Tested-by: Michael Blow <michael.blow@couchbase.com>
    Reviewed-by: Michael Blow <michael.blow@couchbase.com>

--- restriction/convert_analytics_branch.sh
+++ restriction/convert_analytics_branch.sh
@@ -14,7 +14,7 @@ case ${UPSTREAM_BRANCH} in
   stabilization-0020a08254) echo "7.2.2";;
   stabilization-40cfb8705b) echo "7.2.5";;
   stabilization-6a10f3f81d) echo "7.6.2";;
-  stabilization-c8b0f90c72) echo "7.6.4";;
+  stabilization-c8b0f90c72) echo "7.6.5";;
   stabilization-1cffa2bc98) echo "columnar-1.0.5";;
   log4jfix-22d4e6a278) echo "6.6.4";;
   log4jfix-5e11053887) echo "7.0.3";;
