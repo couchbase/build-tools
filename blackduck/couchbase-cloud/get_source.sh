36671753154581da2cb009e35a3fbd5bce47e7e7
Ming Ho <ming.ho@couchbase.com>
2025-05-22 08:32:28 -0700

    CBD-6347, fix workflow path that is used to obtain golang version.
    -Ming
    
    Change-Id: Ia8a51cfb923f8287708516c5c343a697bba5faf5
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/228173
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/couchbase-cloud/get_source.sh
+++ blackduck/couchbase-cloud/get_source.sh
@@ -4,13 +4,13 @@ pushd couchbase-cloud
 git checkout production
 
 # Use the same go version used on self hosted runners
-GO_VER=$(yq '.inputs.go-version.default' couchbase-cloud/.github/actions/setup-go/action.yml)
+GO_VER=$(yq '.inputs.go-version.default' .github/actions/setup-go/action.yml)
 cbdep install -d "${WORKSPACE}/extra" golang ${GO_VER}
 
 export GONOSUMDB="github.com/prometheus/node_exporter"
 
 # Get all node versions mentioned in build-and-deliver-predev action and install the latest
-export NODE_VERSIONS=$(grep -oP '(?<=node-version: )\S+' couchbase-cloud/.github/workflows/build-and-deliver-predev.yml | sed 's/[^0-9.]//g' | sort -V)
+export NODE_VERSIONS=$(grep -oP '(?<=node-version: )\S+' .github/workflows/build-and-deliver-predev.yml | sed 's/[^0-9.]//g' | sort -V)
 NODE_VER=$(echo "${NODE_VERSIONS}" | tail -n 1)
 cbdep install -d "${WORKSPACE}/extra" nodejs ${NODE_VER}
 
