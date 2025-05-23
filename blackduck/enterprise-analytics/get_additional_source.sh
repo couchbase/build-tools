b3bdb7397b2094677e040f63733316c80296be3b
Ming Ho <ming.ho@couchbase.com>
2025-05-23 11:53:09 -0700

    CBD-6351 scripts for enterprise-analytics Blackduck scan.
    -Ming
    
    Change-Id: I99e024ade5b3f1004bb6564411e1fd0e44eb458c
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/228193
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

---
+++ blackduck/enterprise-analytics/get_additional_source.sh
@@ -0,0 +1,96 @@
+#!/bin/bash -ex
+
+RELEASE=$1
+VERSION=$2
+BLD_NUM=$3
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_DIR="${WORKSPACE}/tempbuild"
+
+NODEJS_VERSION=16.20.2
+
+create_analytics_poms() {
+  # We need to ask Analytics to build us a BOM, which we then convert
+  # to a series of poms that Black Duck can scan. Unfortunately this
+  # requires actually building most of Analytics. However, it does
+  # allow us to bypass having Detect scan the analytics/ directory.
+  pushd "${BUILD_DIR}"
+  ninja analytics
+  popd
+
+  mkdir -p analytics-boms
+  uv run --project "${SCRIPT_DIR}/../scripts" --quiet \
+    "${SCRIPT_DIR}/../scripts/create-maven-boms.py" \
+      --outdir analytics-boms \
+      --file analytics/cbas/cbas-install/target/bom.txt
+}
+
+# Main script starts here - decide which action to take based on VERSION
+
+# Have to configure the Server build for several reasons. Couldn't find
+# a reliable way to extract the CMake version from the source (because
+# CMake downloads themselves are inconsistent), so just hardcode a
+# recent CMake.
+CMAKE_VERSION=3.29.6
+NINJA_VERSION=1.10.2
+cbdep install -d "${WORKSPACE}/extra" cmake ${CMAKE_VERSION}
+cbdep install -d "${WORKSPACE}/extra" ninja ${NINJA_VERSION}
+export PATH="${BUILD_DIR}/tlm/deps/maven.exploded/bin:${WORKSPACE}/extra/cmake-${CMAKE_VERSION}/bin:${WORKSPACE}/extra/ninja-${NINJA_VERSION}/bin:${PATH}"
+export CB_MAVEN_REPO_LOCAL=~/.m2/repository
+
+rm -rf "${BUILD_DIR}"
+mkdir "${BUILD_DIR}"
+pushd "${BUILD_DIR}"
+LANG=en_US.UTF-8 cmake -G Ninja "${WORKSPACE}/src" -DBUILD_COLUMNAR=true -DBUILD_ENTERPRISE=true
+
+# Most cbdeps packages that have embedded black-duck-manifest.yaml files will
+# be under ${BUILD_DIR} and so will get picked up automatically. However, cbpy
+# gets unpacked into the install directory, which we will shortly delete. Copy
+# that file into ${BUILD_DIR} to keep it safe, if it exists.
+CBPY_MANIFEST="${WORKSPACE}/src/install/lib/python/interp/cbpy-black-duck-manifest.yaml"
+if [ -e "${CBPY_MANIFEST}" ]; then
+  cp "${CBPY_MANIFEST}" .
+fi
+
+# Newer cbpy packages have a locked requirements.txt file that Black
+# Duck can parse directly. If that exists, copy it to the source
+# directory. Either way, ensure no other requirements.txt files are
+# present.
+find "${WORKSPACE}" -type f -name requirements.txt -delete
+CBPY_REQS="${WORKSPACE}/src/install/lib/python/interp/lib/cb-requirements.txt"
+if [ -e "${CBPY_REQS}" ]; then
+  cp "${CBPY_REQS}" "${WORKSPACE}/src/requirements.txt"
+else
+  # If there's no locked requirements.txt, we need to create one
+  # or else Detect complains
+  touch "${WORKSPACE}/src/requirements.txt"
+fi
+
+# Extract the set of Go versions from the build.
+GO_MANIFEST="${WORKSPACE}/src/enterprise-analytics-black-duck-manifest.yaml"
+GOVER_FILE=$(ls tlm/couchbase-server-*-go-versions.yaml)
+uv run --project "${SCRIPT_DIR}/../scripts" --quiet \
+  "${SCRIPT_DIR}/../scripts/build-go-manifest.py" \
+    --go-versions "${GOVER_FILE}" \
+    --output "${GO_MANIFEST}" \
+    --max-ver-file max-go-ver.txt
+
+# Also install the maximum Golang version and put it on PATH for later
+GOMAX=$(cat max-go-ver.txt)
+cbdep install -d "${WORKSPACE}/extra" golang ${GOMAX}
+export PATH="${WORKSPACE}/extra/go${GOMAX}/bin:${PATH}"
+
+popd
+
+create_analytics_poms
+
+# Black Duck does a number of "go" operations directly, which requires
+# that all packages are fully tidied.
+pushd "${BUILD_DIR}"
+ninja go-mod-tidy-all
+popd
+
+# Delete all the built artifacts so BD doesn't scan them. Do this last
+# as some of the earlier steps may depend on things in the install
+# directory, eg. python.
+rm -rf install
---
+++ blackduck/enterprise-analytics/prune_source.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -ex
+
+RELEASE=$1
+VERSION=$2
+BLD_NUM=$3
+TOOLS_DIR=$4
+
+shopt -s extglob
+
+# NPM-related stuff - see CBD-3365
+
+# Most of this stuff is npm-generated "compiled" code; we ONLY want
+# to scan the npm package.json/package-lock.json files
+mv ns_server/priv/public/ui/package*.json ns_server/priv
+rm -rf ns_server/priv/public
+
+# Server doesn't use asterix's dashboard, so prune that
+rm analytics/asterixdb/asterixdb/asterix-dashboard/src/node/package.json
+
+# This is build-time only, not shipped. (The directory name has changed over
+# time.)
+rm -f query-ui/query-ui/n1ql_parser/package.json
+rm -f query-ui/query-ui/parser/antlr_runtime/package.json
+
+# END NPM-related stuff
+
+# Need to save any of these before deleting the rest of cbbuild
+find cbbuild -name couchbase-server-black-duck-manifest.yaml -print0 | xargs -0 -I FILENAME cp FILENAME .
+
+# cleanup unwanted stuff - test files, build scripts, etc.
+rm -rf testrunner
+rm -rf cbbuild
+rm -rf goproj/src/github.com/couchbase/query/data/sampledb
+rm -rf goproj/src/github.com/couchbase/docloader/examples
+rm -rf goproj/src/github.com/couchbase/indexing/secondary/docs
+find . -name rebar -print0 | xargs -0 rm -rf
+
+# General-purpose removal of test data, examples, docs, etc.
+find . -name analytics -prune -o -name regulator -prune -o -type d -name test\* -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name testdata -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name gtest -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name testing -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name \*tests -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -name backup -prune -o -name indexing -prune -o -type d -name data -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name docs -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name example -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name examples -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name samples -print0 | xargs -0 rm -rf
+find . -name analytics -prune -o -type d -name benchmarks -print0 | xargs -0 rm -rf
+
+# Remove all msvc, vcs* window projects
+WIN='example *msvc* *vcproj* *vcxproj* visual vstudio dot_net_example example csharp vc7ide'
+for windir in ${WIN}; do
+    find . -name analytics -prune -o -type d -name "$windir" -print0 | xargs -0 rm -rf
+done
