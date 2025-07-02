0dcfabbdbd9e91663780d8316a935fd8c09844c2
Chris Hillery <ceej@couchbase.com>
2025-07-02 15:50:40 -0700

    CBD-5948, convert vuln scripts to use latest BD rest apis.
    -Ming
    
    Change-Id: I774ad2089b82c6af69d89d42ce587c57cfaeda96
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/222380
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- blackduck/jira-integration/blackduck_client.py
+++ blackduck/jira-integration/blackduck_client.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 from pathlib import Path
 from itertools import groupby
 import urllib
-from blackduck.HubRestApi import HubInstance
+from blackduck import Client
 import constants
 
 # Set up logging
@@ -15,6 +15,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
+
 
 class BlackduckClient:
     def __init__(self):
@@ -29,28 +30,77 @@ class BlackduckClient:
             logging.error('Unable to locate blackduck-creds.json')
             sys.exit(1)
 
-        self.hub = HubInstance(
-            bd_creds['url'],
-            bd_creds['username'],
-            bd_creds['password'],
-            insecure=True
+        self.base_url = bd_creds['url']
+        self.hub_client = Client(
+            token=bd_creds['token'],
+            base_url=bd_creds['url'],
+            verify=True,
+            timeout=30.0,
+            retries=5
         )
 
-    def get_bom_files(self, version_url):
+    def _get_resource_by_name(self, key, resource_type,
+                              resource_name, parent=None):
+        '''Helper function to query and return single resource by name'''
+        params = {
+            'q': [f"{key}:{resource_name}"]
+        }
+        result = [r for r in self.hub_client.get_resource(
+            resource_type,
+            parent=parent,
+            params=params)
+            if r[key] == resource_name]
+        if len(result) != 1:
+            raise ValueError(
+                f"Expected one {resource_type} of {resource_name}, but found {len(result)}")
+        return result[0]
+
+    def _fetch_all_links(self, url, headers=None):
+        '''Helper function to fetch paginated data from the given URL,'''
+        all_items = []
+        while url:
+            result = self.hub_client.get_json(url, headers=headers)
+            # Add items from the current page to the list
+            all_items.extend(result.get('items', []))
+
+            # Look for the "paging-next" link in the links
+            url = next(
+                (link['href'] for link in result.get(
+                    '_meta',
+                    {}).get(
+                    'links',
+                    []) if link.get('rel') == 'paging-next'),
+                None)
+        return all_items
+
+    def get_project_by_name(self, project_name):
+        '''Query project by name'''
+        project = self._get_resource_by_name('name', 'projects', project_name)
+        return project
+
+    def get_project_version(self, project, version_name):
+        '''Query project version by name'''
+        version = self._get_resource_by_name(
+            'versionName', 'versions', version_name, project)
+        return version
+
+    def get_bom_files(self, version):
         '''Produce a dictionary of files associated with components in a project version.'''
         component_files = defaultdict(list)
-        url = f"{version_url}/matched-files?limit=3000"
         local_file_prefix = 'file:///home/couchbase/workspace/blackduck-detect-scan/src/'
+        version_url = version['_meta']['href']
+        url = f"{version_url}/matched-files?limit=100"
+        headers = {
+            'Accept': 'application/vnd.blackducksoftware.bill-of-materials-6+json'
+        }
+        items = self._fetch_all_links(url, headers)
 
-        response = self.hub.execute_get(url)
-        if response.status_code == 200:
-            for item in response.json().get('items', []):
-                filepath = item.get('declaredComponentPath') or item.get(
-                    'uri', '')[len(local_file_prefix):]
-                component_url = item['matches'][0]['component'].rsplit(
-                    '/origins', 1)[0]
-                component_files[component_url].append(filepath)
-
+        for item in items:
+            filepath = item.get('declaredComponentPath') or item.get(
+                'uri', '')[len(local_file_prefix):]
+            component_url = item['matches'][0]['component'].rsplit(
+                '/origins', 1)[0]
+            component_files[component_url].append(filepath)
         return component_files
 
     def group_vulnerability_entries(self, cve_list, component_files):
@@ -115,49 +165,34 @@ class BlackduckClient:
 
     def get_bom_vulns(self, version):
         '''Retrieve vulnerabilities associated with a specific project version from Blackduck.'''
-        limit, offset, vulns = 1000, 0, []
-        bom_url = f"{version['_meta']['href']}/vulnerable-bom-components"
-        bom_headers = {
-            'Accept': 'application/vnd.blackducksoftware.bill-of-materials-6+json'}
-
-        while True:
-            response = self.hub.execute_get(
-                f'{bom_url}?limit={limit}&offset={offset}',
-                custom_headers=bom_headers)
-            if response.status_code != 200:
-                logging.error(
-                    f"Failed to fetch vulnerabilities: {response.text}")
-                break
-            vulns.extend(response.json().get('items', []))
-            offset += limit
-            if len(vulns) >= response.json().get('totalCount', 0):
-                break
-
+        vulns = []
+        bom_url = f"{version['_meta']['href']}/vulnerable-bom-components?limit=100"
+        headers = {
+            'Accept': 'application/vnd.blackducksoftware.bill-of-materials-8+json'
+        }
+        items = self._fetch_all_links(bom_url, headers)
+        vulns.extend(items)
         return vulns
 
     def prepare_vulnerability_entries(self, version):
         '''Prepare vulnerability entries for reporting.'''
-        url = version['_meta']['href']
-        component_files = self.get_bom_files(url)
-        raw_entries = self.get_bom_vulns(version)
+        component_files = self.get_bom_files(version)
+        entries = self.get_bom_vulns(version)
 
         cve_list = []
-        for entry in raw_entries:
-            cve_name = entry['vulnerabilityWithRemediation']['vulnerabilityName']
-            # Skip if CVE is in the exclusion list
-            if cve_name in constants.EXCLUDED_CVE_LIST:
-                continue
-            cve_detail = self.hub.get_vulnerabilities(cve_name)
+        for entry in entries:
+            cve_name = entry['vulnerability']['vulnerabilityId']
+            url = f"{self.base_url}/api/vulnerabilities/{cve_name}"
+            cve_detail = self.hub_client.get_json(url)
             cve_link = next(
                 (x['href'] for x in cve_detail['_meta']['links'] if x['rel'] == 'nist'), '')
-
             cve_list.append({
                 'componentVersion': entry['componentVersion'],
                 'componentName': entry['componentName'],
                 'componentVersionName': entry['componentVersionName'],
                 'cve_name': cve_name,
-                'severity': entry['vulnerabilityWithRemediation']['severity'],
-                'updatedDate': entry['vulnerabilityWithRemediation']['vulnerabilityUpdatedDate'],
+                'severity': cve_detail['severity'],
+                'updatedDate': cve_detail['updatedDate'],
                 'nist': cve_link
             })
 
@@ -168,13 +203,8 @@ class BlackduckClient:
         url = f"{version['_meta']['href']}/bom-status"
         headers = {
             'Accept': 'application/vnd.blackducksoftware.bill-of-materials-6+json'}
-        response = self.hub.execute_get(url, custom_headers=headers)
-
-        if response.status_code == 200:
-            return response.json()
-        else:
-            logging.error('Unable to determine scan status.')
-            sys.exit(1)
+        result = self.hub_client.get_json(url, headers=headers)
+        return result
 
     def get_version_journal(self, version, start_date):
         '''Retrieve journal entries from blackduck_hub updates for a project version.
@@ -190,8 +220,7 @@ class BlackduckClient:
         date_string = start_date.isoformat()
         encoded_date_string = urllib.parse.quote(date_string)
 
-        cve_entries = []
-        removed_entries = []
+        cve_entries, removed_entries = [], []
         url_base = version['_meta']['href'].replace(
             'api', 'api/journal')
         url = (
@@ -202,16 +231,16 @@ class BlackduckClient:
             f"&filter=journalAction%3Acomponent_deleted"
             f"&limit=1000"
         )
-        journal_headers = {
+        headers = {
             'Accept': 'application/vnd.blackducksoftware.journal-4+json'}
-        activities = self.hub.execute_get(url, custom_headers=journal_headers)
+        activities = self.hub_client.get_json(url, headers=headers)
 
-        if activities.json().get('totalCount') == 0:
+        if activities.get('totalCount') == 0:
             logging.info(
                 'No vulnerability updates from Black Duck Hub since the last scan.')
             return [], []
 
-        journal_entries = activities.json().get('items', [])
+        journal_entries = activities.get('items', [])
         for entry in journal_entries:
             if entry['action'] == 'Vulnerability Found':
                 cve_name=entry['currentData']['vulnerabilityId']
