73fbf88496c206967cf9cc1795be1ac2fae473c6
Chris Hillery <ceej@couchbase.com>
2025-07-15 17:16:56 -0700

    CBD-6342: Add prometheus itself to cbdeps::prometheus BD scan
    Change-Id: I5b15e26bd6914d4556fe443fb5febc86039f752f
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/230863
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>

--- blackduck/cbdeps/prometheus/get_additional_source.sh
+++ blackduck/cbdeps/prometheus/get_additional_source.sh
@@ -3,10 +3,22 @@
 PACKAGE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${PACKAGE_DIR}/../../../utilities/shell-utils.sh"
 
+VERSION=$2
+
 # This will also create
 # `src/cbdeps::prometheus-black-duck-manifest.yaml` containing an entry
 # for "Go programming language" with the appropriate version.
 export PATH="$(${WORKSPACE}/build-tools/blackduck/jenkins/util/go-path-from-manifest.sh):$PATH"
+
+# We also need to add Prometheus *itself* to the Black Duck Manifest.
+# This script is configuring the BD scan for the project
+# "cbdeps::prometheus", which in turn depends on the BD component
+# prometheus/prometheus, as well as all of its dependencies.
+cat <<EOF > "${WORKSPACE}/src/${PRODUCT}-black-duck-manifest.yaml"
+  prometheus:
+    bd-id: 04b82d3f-8119-4a41-bfb6-e71168773767
+    versions: [ "v${VERSION}" ]
+EOF
 
 # Install NodeJS
 NODEJS_VER=$(annot_from_manifest NODEJS_VERSION)
