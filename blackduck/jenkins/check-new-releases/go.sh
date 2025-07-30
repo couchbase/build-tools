10d931116379bcade97ba257adde62973aed850a
Blair Watt <blair.watt@couchbase.com>
2025-07-30 17:25:50 +0100

    CBD-5608: Exit with return code=1 if changes being proposed
    Change-Id: I6ef46a36d95d18b4c374465e7d8bdd54322da509
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/231581
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- blackduck/jenkins/check-new-releases/go.sh
+++ blackduck/jenkins/check-new-releases/go.sh
@@ -2,8 +2,16 @@
 
 uv run check_new_releases.py
 
-if [ ! -z "$(git status --porcelain)" ] && [ "$1" == "--push" ]; then
-  git remote add gerrit ssh://${GERRIT_USER}@review.couchbase.org:29418/build-tools
-  git commit -am "Blackduck: add missing versions"
-  git push gerrit HEAD:refs/for/master
+if [ ! -z "$(git status --porcelain)" ]; then
+  if [ "$1" == "--push" ]; then
+    git remote add gerrit ssh://${GERRIT_USER}@review.couchbase.org:29418/build-tools
+    git commit -am "Blackduck: add missing versions"
+    git push gerrit HEAD:refs/for/master
+    echo "Changes have been pushed to Gerrit. Please review the changes at https://review.couchbase.org"
+    exit 1
+  else
+    echo "Changes detected but not pushing. Run with --push to submit to Gerrit."
+  fi
+else
+  echo "No changes detected."
 fi
