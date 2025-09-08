cb5ed176beebc0c10a11b55b0d8b8ec03b6c9dda
Chris Hillery <ceej@couchbase.com>
2025-09-08 13:45:10 -0700

    Use managed python version for couchbase-tableau-connector build
    The Tableau Connector SDK installs unversioned Python dependencies, and
    those dependencies now require Python 3.9 which isn't available on the
    older Tableau Connector build agent. Update the build script to install
    Python using `uv` so we have direct control over the version.
    
    Also update to use much newer version of Maven.
    
    Change-Id: I47352b9a20a2db323a1f7e92b6cb09a4e1c491e4
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/233476
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Peeyush Gupta <peeyush.gupta@couchbase.com>

--- couchbase-tableau-connector/tableau-connector-build.sh
+++ couchbase-tableau-connector/tableau-connector-build.sh
@@ -1,24 +1,28 @@
 #!/bin/bash -ex
 
 echo "Download dependent tools: maven and jdk"
+
 #When set JDK_HOME to system installed, it didn't seem to work somehow.
 #Download via cbdep so we have control over which version to use.
 #Download maven (3.3.9+ should work)
 
-CBDEP_VESION=1.1.2
 JDK_VERSION=11.0.20+8
-MAVEN_VERSION=3.5.2-cb6
+MAVEN_VERSION=3.9.11
+PYTHON_VERSION=3.11.13
 
-mkdir deps
-mkdir dist
+rm -rf deps && mkdir deps
+rm -rf dist && mkdir dist
 
 pushd deps
-curl https://packages.couchbase.com/cbdep/${CBDEP_VESION}/cbdep-${CBDEP_VESION}-linux-x86_64 -o cbdep
-chmod +x cbdep
-./cbdep install openjdk ${JDK_VERSION} -d .
-./cbdep install maven ${MAVEN_VERSION} -d .
-export PATH=$(pwd)/maven-${MAVEN_VERSION}/bin:$(pwd)/openjdk-${JDK_VERSION}/bin:$PATH
+cbdep install openjdk ${JDK_VERSION} -d .
+cbdep install mvn ${MAVEN_VERSION} -d .
+export PATH=$(pwd)/mvn-${MAVEN_VERSION}/bin:$(pwd)/openjdk-${JDK_VERSION}/bin:$PATH
 export JAVA_HOME=$(pwd)/openjdk-${JDK_VERSION}
+
+# Also create a uv-managed python venv for tableau-connector-sdk
+uv venv --python ${PYTHON_VERSION} --managed-python python-${PYTHON_VERSION}
+export PY_EXE=$(pwd)/python-${PYTHON_VERSION}/bin/python3
+
 popd
 
 #Call maven target to replace the *-SNAPSHOT version with ${VERSION} in the pom
@@ -32,7 +36,7 @@ mvn -B versions:set \
 #DIGICERT_PASSWORD is an environment variable injected into jenkins job.
 
 mvn -B install -DskipTests \
-    -Dpython.path=$(which python3) \
+    -Dpython.path=${PY_EXE} \
     -DproductVersion=${VERSION}-${BLD_NUM} \
     -Djarsigner.arguments=-tsa,http://timestamp.digicert.com \
     -Dtaco.sign \
