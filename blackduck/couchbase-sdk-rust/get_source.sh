d0a3b75ca09460e98252eb48eb204832906e70e9
Charles Dixon <chvckd@gmail.com>
2025-10-06 14:13:07 +0100

    [Black Duck] Add support for Rust SDK
    Change-Id: Ie43f17ae95bf185dc59370d0d71f2a61699609aa
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/234774
    Tested-by: Charles Dixon <chvckd@gmail.com>
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>

---
+++ blackduck/couchbase-sdk-rust/get_source.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+PRODUCT=$1
+RELEASE=$2
+VERSION=$3
+BLD_NUM=$4
+
+git clone ssh://git@github.com/couchbaselabs/couchbase-rs.git
+pushd couchbase-rs
+if git rev-parse --verify --quiet ${VERSION} >& /dev/null
+then
+    echo "Tag ${VERSION} exists, checking it out"
+    git checkout ${VERSION}
+else
+    echo "No tag ${VERSION}, assuming main"
+fi
+popd
