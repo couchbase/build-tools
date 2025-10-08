ba25844871a43b83b2d90964afdfa467438bf1e2
Blair Watt <blair.watt@couchbase.com>
2025-10-08 09:07:19 +0100

    CBD-6480: Ensure Rust is present for rust sdk scans
    Change-Id: I9d15c488cce6104d74580352f71af88078508d53
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/234849
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- blackduck/couchbase-sdk-rust/get_source.sh
+++ blackduck/couchbase-sdk-rust/get_source.sh
@@ -6,6 +6,12 @@ VERSION=$3
 BLD_NUM=$4
 
 git clone ssh://git@github.com/couchbaselabs/couchbase-rs.git
+
+RUST_VERSION=$(curl -s https://static.rust-lang.org/dist/channel-rust-stable.toml | awk '/^\[pkg.rust\]/{flag=1} flag && /^version =/{print; exit}' | sed 's/version = "\(.*\)"/\1/' | cut -d' ' -f1)
+
+cbdep install -d "${WORKSPACE}/extra" rust ${RUST_VERSION}
+export PATH="${WORKSPACE}/extra/rust-${RUST_VERSION}/bin:${PATH}"
+
 pushd couchbase-rs
 if git rev-parse --verify --quiet ${VERSION} >& /dev/null
 then
