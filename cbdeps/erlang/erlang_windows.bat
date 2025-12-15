161dada6db68a5ca4d3d73859d5c37da96f026b1
Blair Watt <blair.watt@couchbase.com>
2025-12-15 15:37:13 +0000

    MB-68463: Use absolute path for wsl call when building Erlang
    wsl is bugged when connected via ssh (like our jenkins workers)
    See: https://github.com/microsoft/WSL/issues/9231
    
    Change-Id: I625ed305bf3d74d2412243da9de1f5ffd1725c71
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/237724
    Tested-by: Blair Watt <blair.watt@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- cbdeps/erlang/erlang_windows.bat
+++ cbdeps/erlang/erlang_windows.bat
@@ -8,7 +8,7 @@ cd %ROOT_DIR%
 rem WSL handles all the heavy lifting now. Easiest to call this script in
 rem the working directory to avoid backslashitis.
 cd build-tools\cbdeps\erlang
-wsl bash -ex erlang_wsl_script.sh '%INSTALL_DIR%'
+"C:\Program Files\WSL\wsl.exe" bash -ex erlang_wsl_script.sh '%INSTALL_DIR%'
 
 :error
 echo Failed with error %ERRORLEVEL%
