a6197aa1ec1edf3f13243fe70a9791f31b535274
Blair Watt <blair.watt@couchbase.com>
2025-12-22 15:00:40 +0000

    MB-68463: Fix Windows line endings, pass vs vars to wsl
    Change-Id: I493612f815f4fd8a4f6bd71d4713ba65fa6b730f
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/237754
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- cbdeps/erlang/erlang_windows.bat
+++ cbdeps/erlang/erlang_windows.bat
@@ -8,6 +8,10 @@ cd %ROOT_DIR%
 rem WSL handles all the heavy lifting now. Easiest to call this script in
 rem the working directory to avoid backslashitis.
 cd build-tools\cbdeps\erlang
+
+rem Pass VS environment to WSL without path translation
+rem These are already set by win32-environment.bat calling vcvarsall.bat
+set WSLENV=INCLUDE:LIB:LIBPATH
 "C:\Program Files\WSL\wsl.exe" bash -ex erlang_wsl_script.sh '%INSTALL_DIR%'
 
 :error
--- cbdeps/erlang/erlang_wsl_script.sh
+++ cbdeps/erlang/erlang_wsl_script.sh
@@ -1,5 +1,8 @@
 INSTALL_DIR=$(wslpath "$1")
 
+set -e
+set -o pipefail
+
 # Expects to be called in the directory containing this script,
 # which is three levels deep in build-tools
 cd ../../..
@@ -14,8 +17,51 @@ OPENSSL_DIR=$(echo "$(pwd)/cbdeps/openssl-"*)
 # Now into the OTP source code
 cd erlang
 
+# Fix line endings - when extracted on Windows, scripts may have CRLF
+# but WSL bash needs LF. Convert shell scripts and autoconf files recursively.
+echo "Converting line endings for shell scripts..."
+# Be specific about autoconf files to avoid breaking other .in files
+find . -type f \( \
+    -name "*.sh" \
+    -o -name "otp_build" \
+    -o -name "configure" \
+    -o -name "config.guess" \
+    -o -name "config.sub" \
+    -o -name "install-sh" \
+    -o -name "install.sh" \
+\) -exec sed -i 's/\r$//' {} \;
+
 # build the source, as per instructions
-eval $(./otp_build env_win32 x64)
+# Set up cross-compilation environment manually. We can't use 'otp_build env_win32'
+# because it corrupts backslashes in INCLUDE/LIB/LIBPATH when echoing them.
+# The VS paths are already set via WSLENV from Windows.
+export ERL_TOP="$PWD"
+export OVERRIDE_TARGET="win32"
+export CONFIG_SUBTYPE="win64"
+export WSLcross="true"
+
+# Patch make_version to not transform "win32" architecture for 64-bit builds.
+# OTP 28+ added logic that changes "win32" -> "x86_64-pc-windows" when CONFIG_SUBTYPE="win64",
+# but this breaks the rebar3 pc plugin's platform detection (it expects "win32" pattern).
+# We need CONFIG_SUBTYPE for 64-bit compiler flags, but we need to preserve the architecture.
+MAKE_VERSION_FILE="erts/emulator/utils/make_version"
+if grep -q 'x86_64-pc-windows' "$MAKE_VERSION_FILE"; then
+    echo "Patching make_version to preserve win32 architecture for x64 builds..."
+    # Comment out just the win64 architecture transformation line
+    sed -i 's|\$architecture = "x86_64-pc-windows";|# PATCHED: keep win32 for rebar3 pc plugin compatibility|' "$MAKE_VERSION_FILE"
+fi
+export CC="cc.sh"
+export CXX="cc.sh"
+export AR="ar.sh"
+export RANLIB="true"
+export OVERRIDE_CONFIG_CACHE_STATIC="$ERL_TOP/make/autoconf/win64.config.cache.static"
+export OVERRIDE_CONFIG_CACHE="$ERL_TOP/make/autoconf/win64.config.cache"
+export WIN32_WRAPPER_PATH="$ERL_TOP/erts/etc/win32/wsl_tools/vc:$ERL_TOP/erts/etc/win32/wsl_tools"
+export PATH="$WIN32_WRAPPER_PATH:$PATH"
+export VCToolsRedistDir=""
+# Build WSLPATH from Windows paths in PATH (for wrapper scripts that need it)
+export WSLPATH=$(echo "$PATH" | tr ':' '\n' | grep '^/mnt/c/' | tr '\n' ':' | sed 's/:$//')
+# INCLUDE, LIB, LIBPATH are already set correctly via WSLENV from Windows
 ./otp_build configure \
       --without-javac \
       --without-et \
