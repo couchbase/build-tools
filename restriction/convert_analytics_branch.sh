8e9483ff3d233cccf9f308fe5e59d18dcb22f84f
Michael Blow <michael.blow@couchbase.com>
2026-02-25 17:41:46 -0500

    MB-67780: += lumina branch mapping
    Change-Id: Iff74072e02f6ed4479e7e3137a3e39dc69b7fccc
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/240788
    Tested-by: Michael Blow <michael.blow@couchbase.com>
    Reviewed-by: Michael Blow <michael.blow@couchbase.com>

--- restriction/convert_analytics_branch.sh
+++ restriction/convert_analytics_branch.sh
@@ -21,6 +21,6 @@ case ${UPSTREAM_BRANCH} in
   stabilization-0795ad31de) echo "ea-2.0.0";;
   log4jfix-22d4e6a278) echo "6.6.4";;
   log4jfix-5e11053887) echo "7.0.3";;
-  morpheus|trinity|neo|cheshire-cat|mad-hatter|master|6.5.x-docs|goldfish|ionic|phoenix) echo "${UPSTREAM_BRANCH}";;
+  lumina|morpheus|trinity|neo|cheshire-cat|mad-hatter|master|6.5.x-docs|goldfish|ionic|phoenix) echo "${UPSTREAM_BRANCH}";;
   *) echo "Don't know how to handle upstream branch: ${UPSTREAM_BRANCH}"; exit 1;;
 esac
