084ef8036a85e9313a1a05b17b6797f3f0237c72
Ming Ho <ming.ho@couchbase.com>
2026-04-13 14:01:59 -0700

    MB-67169, include additional cuda header files in gpu package.
    instead of including specific headers, include all headers from cuda distro.
    
    -Ming
    
    Change-Id: I0af205d86d33163e84db8b8a7fd0c88de6446609
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/243325
    Reviewed-by: Abhi Dangeti <abhinav@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- cbdeps/faiss/faiss_unix.sh
+++ cbdeps/faiss/faiss_unix.sh
@@ -140,17 +140,12 @@ if [[ "${OS_TYPE}" == "darwin" ]]; then
     popd
 fi
 
-# package cuda libraries when building with ENABLE_GPU
+# package cuda libraries and header files when building with ENABLE_GPU
 if [[ "${ENABLE_GPU}" == "ON" ]]; then
     cp -av ${CUDA_HOME}/lib64/libcudart.so* ${INSTALL_DIR}/lib/.
     cp -av ${CUDA_HOME}/lib64/libcublas.so* ${INSTALL_DIR}/lib/.
     cp -av ${CUDA_HOME}/lib64/libcublasLt.so* ${INSTALL_DIR}/lib/.
-
-    cp -av ${CUDA_HOME}/include/cuda_runtime.h ${INSTALL_DIR}/include/.
-    cp -av ${CUDA_HOME}/include/cuda_runtime_api.h ${INSTALL_DIR}/include/.
-    cp -av ${CUDA_HOME}/include/cublas_v2.h ${INSTALL_DIR}/include/.
-    cp -av ${CUDA_HOME}/include/cublas.h ${INSTALL_DIR}/include/.
-    cp -av ${CUDA_HOME}/include/cublasLt.h ${INSTALL_DIR}/include/.
+    cp -av ${CUDA_HOME}/include/* ${INSTALL_DIR}/include/.
 fi
 # Create BD_MANIFEST if requested
 if [ -n "${BD_MANIFEST}" ]; then
