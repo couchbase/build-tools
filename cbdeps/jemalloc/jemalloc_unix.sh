1cc2324705f3463537b5fd466024b3574a2cca85
Jim Walker <jim@couchbase.com>
2026-09-08 12:42:40 +0100

    MB-73788: Rebuild arch64 linux with 4k pagesize
    Match the old dependency in this regard to avoid any risk of
    bloating RSS in upgraded environments.
    
    Change-Id: Iadec8903d9c077e7f68f7ae68b0cd85489123f79
    Reviewed-on: https://review.couchbase.org/c/build-tools/+/252793
    Reviewed-by: Jim Walker <jim@couchbase.com>
    Tested-by: Jim Walker <jim@couchbase.com>

--- cbdeps/jemalloc/jemalloc_unix.sh
+++ cbdeps/jemalloc/jemalloc_unix.sh
@@ -16,11 +16,19 @@ VERSION=$6
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# On linux/aarch64 force a 4K page size, otherwise use jemalloc's default.
+# MB-73788.
+PAGECONF=
+if [ "${PLATFORM}" = "linux" ] && [ "$(uname -m)" = "aarch64" ]; then
+    PAGECONF="--with-lg-page=12"
+fi
+
 # Build the main installation with je_ symbol prefix
 "${SCRIPT_DIR}/scripts/build_jemalloc.sh" \
    "${ROOT_DIR}" \
    "--with-jemalloc-prefix=je_ --disable-cache-oblivious --disable-zone-allocator \
-   --disable-initial-exec-tls --disable-cxx --with-lg-tcache-limit=15" \
+   --disable-initial-exec-tls --disable-cxx --with-lg-tcache-limit=15 \
+   ${PAGECONF}" \
    "${INSTALL_DIR}" \
    "" \
    "${VERSION}"
